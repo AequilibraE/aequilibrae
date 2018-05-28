44550b03d1037268e257cc223ce35e37fcddbbfe
Pedro Camargo <veigacamargo@gmail.com>
2018-05-28 21:58:02 +1000

    Packaging work

--- aequilibrae/__init__.py
+++ aequilibrae/__init__.py
@@ -3,10 +3,14 @@
 Core AequilibraE
 =============
 
-Imports AequilibraE modules
+Imports aequilibrae modules
 
 """
+
+name = "aequilibrae"
+
 import sys
+
 sys.dont_write_bytecode = True
 
 from . import paths
@@ -17,7 +21,6 @@ from . import transit
 import reserved_fields
 from parameters import Parameters
 from .reference_files import spatialite_database
-
 
 from parameters import Parameters
 import logging
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aequilibrae",
+    version="0.4.2",
+    author="Pedro Camargo",
+    author_email="pedro@xl-optim.com",
+    description="A package for transportation modeling",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AequilibraE/pyquilibrae",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
