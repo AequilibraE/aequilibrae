2a2176cf7e1554d9bf41e9e2bf3dd5847437e216
Pedro Camargo <veigacamargo@gmail.com>
2018-06-04 21:18:32 +1000

    Update to setup and CI

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://github.com/AequilibraE/pyquilibrae",
     packages=setuptools.find_packages(),
     classifiers=(
-        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
