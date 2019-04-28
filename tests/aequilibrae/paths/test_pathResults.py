88eeaef75f404c5a3526e0afa98f87bfdacc3e7e
Pedro Camargo <veigacamargo@gmail.com>
2019-04-28 23:58:29 +1000

    Fixes formats

--- tests/aequilibrae/paths/test_pathResults.py
+++ tests/aequilibrae/paths/test_pathResults.py
@@ -1,12 +1,14 @@
+import os
+import sys
 from unittest import TestCase
-import sys, os
+
 from aequilibrae.paths import path_computation, Graph
 from aequilibrae.paths.results import PathResults
+from ...data import test_graph
 
 # Adds the folder with the data to the path and collects the paths to the files
 lib_path = os.path.abspath(os.path.join("..", "../tests"))
 sys.path.append(lib_path)
-from ...data import path_test, test_graph
 
 origin = 5
 dest = 27
@@ -22,22 +24,22 @@ class TestPathResults(TestCase):
         self.r = PathResults()
         try:
             self.r.prepare(self.g)
-        except:
-            self.fail("Path result preparation failed")
+        except Exception as err:
+            self.fail("Path result preparation failed - {}".format(err.__str__()))
 
     def test_reset(self):
         self.test_prepare()
         try:
             self.r.reset()
-        except:
-            self.fail("Path result resetting failed")
+        except Exception as err:
+            self.fail("Path result resetting failed - {}".format(err.__str__()))
 
     def test_update_trace(self):
         self.test_prepare()
         try:
             self.r.reset()
-        except:
-            self.fail("Path result resetting failed")
+        except Exception as err:
+            self.fail("Path result resetting failed - {}".format(err.__str__()))
 
         path_computation(origin, dest, self.g, self.r)
 
