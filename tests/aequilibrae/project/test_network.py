84ab3eaa92187da4760ad3c94adcae4a9cba0ca6
pedro Camargo <c@margo.co>
2019-12-28 21:22:12 +1030

    testing for creation of empty tables

--- tests/aequilibrae/project/test_network.py
+++ tests/aequilibrae/project/test_network.py
@@ -5,6 +5,7 @@ import os
 import shutil
 import platform
 from time import sleep
+from functools import reduce
 from aequilibrae.project.network.network import Network
 from aequilibrae.parameters import Parameters
 from aequilibrae.reference_files import spatialite_database
@@ -63,7 +64,7 @@ class TestNetwork(TestCase):
 
     def tearDown(self) -> None:
         self.conn.close()
-        os.unlink(self.file)
+        # os.unlink(self.file)
 
     def test_create_from_osm(self):
         self.network.create_from_osm(west=-112.185, south=36.59, east=-112.179, north=36.60, modes=["car"])
@@ -85,8 +86,40 @@ class TestNetwork(TestCase):
         if lks > nds:
             self.fail("We imported more links than nodes. Something wrong here")
 
-    def test_count_links(self):
-        self.fail()
+    def test_create_empty_tables(self):
+        self.network.create_empty_tables()
+        p = Parameters().parameters["network"]
 
-    def test_count_nodes(self):
-        self.fail()
+        curr = self.conn.cursor()
+        curr.execute("""PRAGMA table_info(links);""")
+        fields = curr.fetchall()
+        fields = [x[1] for x in fields]
+
+        oneway = reduce(lambda a, b: dict(a, **b), p["links"]["fields"]["one-way"])
+        owf = list(oneway.keys())
+        twoway = reduce(lambda a, b: dict(a, **b), p["links"]["fields"]["two-way"])
+        twf = []
+        for k in list(twoway.keys()):
+            twf.extend(["{}_ab".format(k), "{}_ba".format(k)])
+
+        for f in owf + twf:
+            if f not in fields:
+                self.fail("Field {} not added to links table".format(f))
+
+        curr = self.conn.cursor()
+        curr.execute("""PRAGMA table_info(nodes);""")
+        nfields = curr.fetchall()
+        nfields = [x[1] for x in nfields]
+
+        flds = reduce(lambda a, b: dict(a, **b), p["nodes"]["fields"])
+        flds = list(flds.keys())
+
+        for f in flds:
+            if f not in nfields:
+                self.fail("Field {} not added to nodes table".format(f))
+
+    # def test_count_links(self):
+    #     self.fail()
+    #
+    # def test_count_nodes(self):
+    #     self.fail()
