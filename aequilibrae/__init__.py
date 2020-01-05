f7b7bd43573e3cc091e6b319f2a91a800969be4f
pedro Camargo <c@margo.co>
2020-01-05 17:40:01 +1100

    new init file

--- aequilibrae/__init__.py
+++ aequilibrae/__init__.py
@@ -1,10 +1,12 @@
 from aequilibrae.starts_logging import StartsLogging
-from aequilibrae import distribution
-from aequilibrae import matrix
-from aequilibrae import paths
-from aequilibrae import transit
-from aequilibrae.paths import release_version as __version__
-from aequilibrae.aequilibrae_project import AequilibraEProject
+from aequilibrae.parameters import Parameters
+
+# from aequilibrae import distribution
+# from aequilibrae import matrix
+# from aequilibrae import paths
+# from aequilibrae import transit
+# from aequilibrae.paths import release_version as __version__
+# from aequilibrae.aequilibrae_project import AequilibraEProject
 
 name = "aequilibrae"
 
