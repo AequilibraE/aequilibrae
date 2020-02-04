0bdec5bf62f56518b825e039e02c72d36ee223de
pedro Camargo <c@margo.co>
2020-02-04 18:31:41 +1000

    Prevents error on import before compilation

--- aequilibrae/paths/__init__.py
+++ aequilibrae/paths/__init__.py
@@ -10,14 +10,14 @@ __date__ = "$Date: 2017-02-25$"
 import warnings
 
 try:
-    from .AoN import one_to_all, skimming_single_origin, path_computation, VERSION_COMPILED, update_path_trace
-    from .results import *
-    from .multi_threaded_aon import MultiThreadedAoN
-    from .multi_threaded_skimming import MultiThreadedNetworkSkimming
-    from .network_skimming import NetworkSkimming
-    from .all_or_nothing import allOrNothing
+    from aequilibrae.paths.AoN import one_to_all, skimming_single_origin, path_computation, VERSION_COMPILED, update_path_trace
+    from aequilibrae.paths.results import *
+    from aequilibrae.paths.multi_threaded_aon import MultiThreadedAoN
+    from aequilibrae.paths.multi_threaded_skimming import MultiThreadedNetworkSkimming
+    from aequilibrae.paths.network_skimming import NetworkSkimming
+    from aequilibrae.paths.all_or_nothing import allOrNothing
+    from aequilibrae.paths.traffic_class import TrafficClass
 except ImportError as e:
     warnings.warn("The AoN extension has not been compiled. {}".format(e.name))
-from .graph import Graph
-from aequilibrae.paths.traffic_class import TrafficClass
+from aequilibrae.paths.graph import Graph
 from .__version__ import binary_version, release_name, minor_version, release_version
