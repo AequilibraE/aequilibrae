62311c29761307b7922b9dfcc5f3b4dcba1aa83d
pedro Camargo <c@margo.co>
2020-02-27 08:47:34 +1000

    Cleans code

--- aequilibrae/project/spatialite_connection.py
+++ aequilibrae/project/spatialite_connection.py
@@ -1,21 +1,16 @@
 import os
-import platform
 from aequilibrae.parameters import Parameters
 from aequilibrae import logger
 
 
 def spatialite_connection(conn):
     conn.enable_load_extension(True)
-    plat = platform.platform()
-    pth = os.getcwd()
-    if "WINDOWS" in plat.upper():
-        par = Parameters()
-        spatialite_path = par.parameters["system"]["spatialite_path"]
-        if os.path.isfile(os.path.join(spatialite_path, "mod_spatialite.dll")):
-            os.chdir(spatialite_path)
+    par = Parameters()
+    spatialite_path = par.parameters["system"]["spatialite_path"]
+    if spatialite_path not in os.environ['PATH']:
+        os.environ['PATH'] = spatialite_path + ';' + os.environ['PATH']
     try:
         conn.load_extension("mod_spatialite")
     except Exception as e:
         logger.warn(f"AequilibraE might not work as intended without spatialite. {e.args}")
-    os.chdir(pth)
     return conn
