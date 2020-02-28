9a1599fa4b896386802682d3a362a29ac97ff4ef
pedro Camargo <c@margo.co>
2020-02-28 16:09:23 +1000

    fixes for omx file in gravity calibration

--- aequilibrae/matrix/aequilibrae_matrix.py
+++ aequilibrae/matrix/aequilibrae_matrix.py
@@ -833,7 +833,8 @@ class AequilibraeMatrix(object):
                                  mappings=self.index_names)
             output = temp.copy(output_name, cores, names, compress)
             if self.view_names is not None:
-                output.computational_view(self.view_names)
+                view_names = [names[self.view_names.index(nm)] for nm in self.view_names]
+                output.computational_view(view_names)
             temp.close()
             del temp
             os.unlink(fp)
@@ -884,6 +885,10 @@ class AequilibraeMatrix(object):
                 output.indices[:] = self.indices[:]
                 for i, c in enumerate(cores):
                     output.matrices[:, :, i] = self.matrices[:, :, self.names.index(c)]
+                if self.view_names is not None:
+                    view_names = [names[self.view_names.index(nm)] for nm in self.view_names]
+                    output.computational_view(view_names)
+
         output.matrix_hash = output.__builds_hash__()
         output.matrices.flush()
         return output
