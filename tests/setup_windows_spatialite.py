be1a75b4011e37024acaeb35a6a3752012654882
pedro Camargo <c@margo.co>
2020-03-10 11:40:19 +1000

    Adds spatialite downloading to the test

--- tests/setup_windows_spatialite.py
+++ tests/setup_windows_spatialite.py
@@ -1,23 +1,15 @@
 import urllib.request
 import zipfile
-from os.path import join
+from os.path import join, dirname
 import os
 
 pth = 'https://github.com/AequilibraE/aequilibrae/releases/download/V0.6.0.post1/mod_spatialite-NG-win-amd64.zip'
 
 
-outfolder = os.path.dirname(os.path.abspath(__file__))
+outfolder = dirname(dirname(os.path.abspath(__file__)))
+outfolder = join(outfolder, 'aequilibrae/project')
+
 dest_path = join(outfolder, "mod_spatialite-NG-win-amd64.zip")
 urllib.request.urlretrieve(pth, dest_path)
 
 zipfile.ZipFile(dest_path).extractall(outfolder)
-
-bin_folder = join(outfolder, 'mod_spatialite-NG-win-amd64')
-print(bin_folder)
-
-import sqlite3
-
-a = sqlite3.connect(join(outfolder, 'trash.db'))
-a.enable_load_extension(True)
-a.load_extension("mod_spatialite")
-
