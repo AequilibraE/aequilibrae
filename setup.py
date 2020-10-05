66193ff1b2d1550df9c35ce9a3fc527f60fdb6fd
pedro Camargo <c@margo.co>
2020-10-05 16:29:52 +1000

    Adds support for Python 3.9

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ if __name__ == "__main__":
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
         ],
         cmdclass={"build_ext": build_ext},
         ext_modules=[ext_module],
