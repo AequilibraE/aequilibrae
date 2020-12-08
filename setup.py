c97b4e92ad6e56f2101abc40cb04227ae2adf1d2
pedro Camargo <c@margo.co>
2020-12-09 00:23:02 +1000

    Documentation

--- setup.py
+++ setup.py
@@ -12,6 +12,10 @@ here = os.path.dirname(os.path.realpath(__file__))
 whole_path = os.path.join(here, "aequilibrae/paths", "AoN.pyx")
 ext_module = Extension("aequilibrae.paths.AoN", [whole_path], include_dirs=[np.get_include()])
 
+whole_path2 = os.path.join(here, "aequilibrae/paths/path_based", "TrafficAssignmentCy.pyx")
+ext_module2 = Extension("aequilibrae.paths.path_based.TrafficAssignmentCy", [whole_path],
+                        include_dirs=[np.get_include()])
+
 pkgs = [pkg for pkg in find_packages()]
 
 pkg_data = {
@@ -20,7 +24,6 @@ pkg_data = {
     "aequilibrae": ["parameters.yml"],
 }
 loose_modules = ["__version__", "parameters"]
-
 
 if __name__ == "__main__":
     setup(
@@ -45,5 +48,5 @@ if __name__ == "__main__":
             "Programming Language :: Python :: 3.8",
         ],
         cmdclass={"build_ext": build_ext},
-        ext_modules=[ext_module],
+        ext_modules=[ext_module, ext_module2],
     )
