91ed9c2ebe4f58f9eb51c3a22c1d6e67cc3439a9
pedro Camargo <c@margo.co>
2020-12-14 20:56:25 +1000

    updates examples

--- docs/examples/plot_moving_nodes.py
+++ docs/examples/plot_moving_nodes.py
@@ -20,31 +20,12 @@ fldr = join(gettempdir(), uuid4().hex)
 project = create_example(fldr)
 
 # %%
-# Let's see what the network looks like
-# We don't recommend plotting it this way for real networks
-links = project.network.links
-
-# We plot the entire network
-curr = project.conn.cursor()
-curr.execute('Select link_id from links;')
-
-for lid in curr.fetchall():
-    geo = links.get(lid[0]).geometry
-    plt.plot(*geo.xy, color='red')
+# Let's move node one from the upper left corner of the image above, a bit to the left and to the bottom
 
 # We also add the node we want to move
 all_nodes = project.network.nodes
-# We can just get one link in specific
+links = project.network.links
 node = all_nodes.get(1)
-plt.plot(*node.geometry.xy, 'ro', color='green')
-
-plt.show()
-# %%
-# Let's move node one from the upper left corner of the image above, a bit to the left and to the bottom
-
-
-
-
 new_geo = Point(node.geometry.x + 0.02, node.geometry.y - 0.02)
 node.geometry = new_geo
 
@@ -67,8 +48,8 @@ plt.plot(*node.geometry.xy, 'ro', color='black')
 
 plt.show()
 
-# Did you notice the links shifted to match the node move?
-
+# Did you notice the links are matching the node?
+# Look at the original network and see how it used to look like
 
 # %%
 
