4b7b8c9162cab28e2149f4d2b9bf7faf95e45ee1
pedro Camargo <c@margo.co>
2021-01-03 20:50:21 +1000

    code cleaning

--- aequilibrae/project/database_specification/tables/attribute_documentation.sql
+++ aequilibrae/project/database_specification/tables/attribute_documentation.sql
@@ -4,5 +4,5 @@ CREATE TABLE  if not exists attributes_documentation (name_table  TEXT NOT NULL,
                                                       UNIQUE (name_table, attribute)
                                                       );
 
-#
+--#
 CREATE INDEX idx_attributes ON attributes_documentation (name_table, attribute);
