363a696a24ce3c98d3a62de27cb6995527f7e7c2
pedro Camargo <c@margo.co>
2021-01-21 08:50:06 +1000

    Incorporates the QGIS connection from within the library

--- aequilibrae/project/database_connection.py
+++ aequilibrae/project/database_connection.py
@@ -1,8 +1,14 @@
 import os
 import sqlite3
+import importlib.util as iutil
 from aequilibrae.project.spatialite_connection import spatialite_connection
 
-ENVIRON_VAR = 'AEQUILIBRAE_PROJECT_PATH'
+spec = iutil.find_spec("qgis")
+inside_qgis = spec is not None
+if inside_qgis:
+    import qgis
+
+ENVIRON_VAR = "AEQUILIBRAE_PROJECT_PATH"
 
 
 def database_connection() -> sqlite3.Connection:
@@ -10,6 +16,9 @@ def database_connection() -> sqlite3.Connection:
         raise FileExistsError("There is no AequilibraE project loaded to connect to")
 
     path = os.environ.get(ENVIRON_VAR)
-    file_name = os.path.join(path, 'project_database.sqlite')
-    conn = spatialite_connection(sqlite3.connect(file_name))
+    file_name = os.path.join(path, "project_database.sqlite")
+    if inside_qgis:
+        conn = qgis.utils.spatialite_connect(file_name)
+    else:
+        conn = spatialite_connection(sqlite3.connect(file_name))
     return conn
