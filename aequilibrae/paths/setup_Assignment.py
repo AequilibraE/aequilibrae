77b6ab7aac95353da3f05d203e9673c74bf7ca3c
Jan Zill <jan.zill@veitchlister.com.au>
2021-03-14 15:29:38 +1000

    clean up

--- aequilibrae/paths/setup_Assignment.py
+++ aequilibrae/paths/setup_Assignment.py
@@ -28,6 +28,9 @@ if "WINDOWS" in platform.platform().upper():
             extra_link_args=["/openmp"],
             include_dirs=[np.get_include(), pa.get_include()],
             language="c++",
+            libraries=pa.get_libraries(),
+            library_dirs=pa.get_library_dirs(),
+            runtime_library_dirs=pa.get_library_dirs(),
         )
     ]
 else:
@@ -39,15 +42,20 @@ else:
             extra_link_args=["-fopenmp"],
             include_dirs=[np.get_include(), pa.get_include()],
             language="c++",
+            libraries=pa.get_libraries(),
+            library_dirs=pa.get_library_dirs(),
+            runtime_library_dirs=pa.get_library_dirs(),
+            define_macros=("_GLIBCXX_USE_CXX11_ABI", "0"),
         )
     ]
     # I got inexplicable segfaults without the following line, see
     # https://arrow.apache.org/docs/python/extending.html# (see end of doc)
-    ext_modules[0].define_macros.append(("_GLIBCXX_USE_CXX11_ABI", "0"))
-
-
-for ext in ext_modules:
-    ext.libraries.extend(pa.get_libraries())
-    ext.library_dirs.extend(pa.get_library_dirs())
+#     ext_modules[0].define_macros.append(("_GLIBCXX_USE_CXX11_ABI", "0"))
+#
+#
+# for ext in ext_modules:
+#     ext.libraries.extend(pa.get_libraries())
+#     ext.library_dirs.extend(pa.get_library_dirs())
+#     ext.runtime_library_dirs.extend(pa.get_library_dirs())
 
 setup(name="AoN", ext_modules=cythonize(ext_modules))
