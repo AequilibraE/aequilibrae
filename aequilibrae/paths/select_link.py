51ca63d4ce9fb341b032ae0f08290036d3313b5e
Jan Zill <jan.zill@veitchlister.com.au>
2021-04-18 16:38:45 +1000

    more select link

--- aequilibrae/paths/select_link.py
+++ aequilibrae/paths/select_link.py
@@ -34,12 +34,15 @@ class SelectLink(object):
         """
         self.table_name = table_name
         self.select_link_id = None
-        self.select_link_id_compressed = None
+        # self.select_link_id_compressed = None
         self.assignment_results = None
+        self.demand_weights = None
+        self.num_iters = None
         self.classes = []
         # class specific stuff
         self.matrices = {}
         self.compressed_graph_correspondences = {}
+        self.select_link_id_compressed = {}
 
     def set_classes(self, classes: List[TrafficClass]) -> None:
         """
@@ -53,11 +56,11 @@ class SelectLink(object):
             raise Exception("Classes need to be unique. Your list of classes has repeated items/IDs")
         self.classes = classes  # type: List[TrafficClass]
 
-    def initialise_matrices(self) -> None:
+    def _initialise_matrices(self) -> None:
         for c in self.classes:
             self.matrices[c.__id__] = np.zeros_like(c.matrix)
 
-    def read_compressed_graph_correspondence(self) -> None:
+    def _read_compressed_graph_correspondence(self) -> None:
         pth = os.environ.get(ENVIRON_VAR)
         path_base_dir = os.path.join(pth, "path_files", self.assignment_results.procedure_id.values[0])
 
@@ -66,7 +69,7 @@ class SelectLink(object):
                 os.path.join(path_base_dir, f"correspondence_c{c.mode}_{c.__id__}.feather")
             )
 
-    def read_assignment_results(self) -> None:
+    def _read_assignment_results(self) -> None:
         conn = database_connection()
         results_df = pd.read_sql(f"SELECT * FROM 'results'", conn)
         conn.close()
@@ -75,7 +78,7 @@ class SelectLink(object):
             len(self.assignment_results) == 1
         ), "{len(self.assignment_results)} assignment result with this table name, need exactly one"
 
-    def calculate_demand_weights(self):
+    def _calculate_demand_weights(self):
         """Each iteration of traffic assignment contributes a certain fraction to the total solutions. This method
         figures out how much so we can calculate select link matrices by weighting paths per iteration."""
 
@@ -87,31 +90,71 @@ class SelectLink(object):
         assignment_report["convergence"] = eval(assignment_report["convergence"])
         assignment_report["setup"] = eval(assignment_report["setup"])
 
-        num_iters = np.max(assignment_report["convergence"]["iteration"])
+        self.num_iters = np.max(assignment_report["convergence"]["iteration"])
 
-        return np.repeat(1.0 / num_iters, num_iters)
+        # FIXME (Jan 18/4/21): depending on assignment method, this should be different
+        self.demand_weights = np.repeat(1.0 / self.num_iters, self.num_iters)
+
+    def _read_path_file(self, iteration, traffic_class, origin):
+
+        pth = os.environ.get(ENVIRON_VAR)
+        path_base_dir = os.path.join(pth, "path_files", self.assignment_results.procedure_id.values[0])
+        base_dir = os.path.join(path_base_dir, f"iter{iteration}", f"path_c{traffic_class.mode}_{traffic_class.__id__}")
+
+        path_o_f = os.path.join(base_dir, f"o{origin}.feather")
+        path_o_index_f = os.path.join(base_dir, f"o{origin}_indexdata.feather")
+
+        path_o = pd.read_feather(path_o_f)
+        path_o_index = pd.read_feather(path_o_index_f)
+
+        return path_o, path_o_index
 
     def run_select_link_analysis(self, link_id: int) -> None:
         assert len(self.classes) > 0, "Need at least one traffic class to run select link analysis, use set_classes"
 
         self.select_link_id = link_id
+        # look up compressed id for each class. Should be the same.
+        for c in self.classes:
+            graph = self.compressed_graph_correspondences[c.__id__]
+            self.select_link_id_compressed[c.__id__] = graph.loc[graph["link_id"] == self.link_id][
+                "__compressed_id__"
+            ].values[0]
 
         # initialise select link matrices to zero
-        self.initialise_matrices()
+        self._initialise_matrices()
 
         # process iterations
-        self.read_assignment_results()
+        self._read_assignment_results()
 
         # read in compressed correspondence, depends on read_assignment results for path lookup
-        self.read_compressed_graph_correspondence()
+        self._read_compressed_graph_correspondence()
 
         # now get weight of each iteration to weight corresponding demand.
         # FIXME (Jan 18/4/21): this is MSA only atm, needs to be implemented
-        self.calculate_demand_weights()
+        self._calculate_demand_weights()
 
         # now process each iteration.
-        # num_iters =
+        num_centroids = self.classes[0].matrix.zones
 
-        for c in self.classes:
-            graph = self.compressed_graph_correspondences[c.__id__]
-            graph.loc[graph["link_id"] == self.link_id]
+        for iteration in self.num_iters:
+            weight = self.demand_weights[iteration]
+            for c in self.classes:
+                comp_link_id = self.select_link_id_compressed[c.__id__]
+                demand_mat = c.matrix.matrix_view
+
+                for origin in range(num_centroids):
+                    path_o, path_o_index = self._read_path_file(iteration, c, origin)
+
+                    # these are the indeces of the path file where the SL appears, so need to turn these into
+                    # destinations by looking up the values in the path file
+                    idx_to_look_up = path_o.loc[path_o.data == comp_link_id].index.values
+
+                    # drop disconnected zones (and intrazonal). Depends on index being ordered.
+                    path_o_index = path_o_index.drop_duplicates(keep="first")
+                    destinations_this_o_and_iter = np.array(
+                        [path_o_index.loc[path_o_index["data"] >= x].index.min() for x in idx_to_look_up]
+                    )
+
+                    self.matrices[c.__id__][origin, destinations_this_o_and_iter] += (
+                        weight * demand_mat[origin, destinations_this_o_and_iter]
+                    )
