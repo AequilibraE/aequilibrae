e04e5dd83f686d7b6a354c14761d149096d499e2
Jan Zill <jan.zill@veitchlister.com.au>
2021-04-21 22:11:49 +1000

    bug fixes

--- aequilibrae/paths/__init__.py
+++ aequilibrae/paths/__init__.py
@@ -3,6 +3,7 @@ from aequilibrae.paths.multi_threaded_aon import MultiThreadedAoN
 from aequilibrae.paths.multi_threaded_skimming import MultiThreadedNetworkSkimming
 from aequilibrae.paths.network_skimming import NetworkSkimming
 from aequilibrae.paths.all_or_nothing import allOrNothing
+from aequilibrae.paths.select_link import SelectLink
 from aequilibrae.paths.traffic_class import TrafficClass
 from aequilibrae.paths.traffic_assignment import TrafficAssignment
 from aequilibrae.paths.vdf import VDF
@@ -11,7 +12,12 @@ from .__version__ import binary_version, release_name, minor_version, release_ve
 from aequilibrae import logger
 
 try:
-    from aequilibrae.paths.AoN import one_to_all, skimming_single_origin, path_computation, VERSION_COMPILED, \
-        update_path_trace
+    from aequilibrae.paths.AoN import (
+        one_to_all,
+        skimming_single_origin,
+        path_computation,
+        VERSION_COMPILED,
+        update_path_trace,
+    )
 except ImportError as ie:
-    logger.warning(f'Could not import procedures from the binary. {ie.args}')
+    logger.warning(f"Could not import procedures from the binary. {ie.args}")
--- aequilibrae/paths/select_link.py
+++ aequilibrae/paths/select_link.py
@@ -2,10 +2,7 @@ import os
 from typing import List
 import numpy as np
 import pandas as pd
-from aequilibrae.paths.graph import Graph
-from aequilibrae.paths import TrafficClass
-from aequilibrae.matrix import AequilibraeMatrix
-from aequilibrae.paths.results import AssignmentResults
+from aequilibrae.paths.traffic_class import TrafficClass
 from aequilibrae import logger
 from aequilibrae.project.database_connection import ENVIRON_VAR
 from aequilibrae.project.database_connection import database_connection
@@ -104,7 +101,7 @@ class SelectLink(object):
             )
 
         sum_of_contribs = np.sum(self.demand_weights)
-        assert np.close(sum_of_contribs, 1.0), f"Contribution of iterations is not one, but {sum_of_contribs}"
+        assert np.allclose(sum_of_contribs, 1.0), f"Contribution of iterations is not one, but {sum_of_contribs}"
 
     def _figure_out_demand_weights_for_linear_approximation(self, assignment_report):
         """ Linear approximation contribution for each iteration. """
@@ -112,20 +109,38 @@ class SelectLink(object):
         # solution^n+1 = alpha^n * sol^n + (1-alpha^n) * direction^n
         # direction = beta_0 * aon + beta_1 * previous_direction + beta_2 * pre_previous_direction
         alphas = assignment_report["convergence"]["alpha"]
-        # beta_0 = assignment_report["convergence"]["beta0"]
-        # beta_1 = assignment_report["convergence"]["beta1"]
-        # beta_2 = assignment_report["convergence"]["beta2"]
+        print(alphas)
+        # betas_0 = assignment_report["convergence"]["beta0"]
+        # betas_1 = assignment_report["convergence"]["beta1"]
+        # betas_2 = assignment_report["convergence"]["beta2"]
 
         self.demand_weights = np.repeat(1.0, self.num_iters)
 
-        if assignment_report["setup"]["Algorithm"] in ["fw", "frank-wolfe"]:
-            for i in range(self.num_iters, 0, -1):
-                alpha = alphas[i]
-                # demand_weights[i] += alpha * direction + (1.0 - alpha) * current
-                self.demand_weights[i] *= alpha
-                self.demand_weights[: i - 1] *= 1.0 - alpha
+        # FIXME (Jan 21/4/21): implement CFW and BFW, this is only valid for FW
+        # can we just multiply previous like below?
+        # if assignment_report["setup"]["Algorithm"] in ["fw", "frank-wolfe"]:
+        for i in range(0, self.num_iters):
+            alpha = alphas[i]
+            # beta_0 = betas_0[i]
+            # beta_1 = betas_1[i]
+            # beta_2 = betas_2[i]
+            # demand_weights[i] += alpha * direction + (1.0 - alpha) * current
+            # for FW: direction = AON, so multiply current weight
+            # for CFW: direction = beta_0 * AON + beta_1 * previous_direction
+            # for BFW: direction = beta_0 * AON + beta_1 * previous_direction + beta_2 * pre_previous_direction
+            self.demand_weights[i] *= alpha
+            self.demand_weights[:i] *= 1.0 - alpha
+
+            # if (assignment_report["setup"]["Algorithm"] in ["cfw", "bfw"]) and (i > 1):
+            #     self.demand_weights[i] *= beta_0
+            #     self.demand_weights[:(i - 1)] *= beta_2
+            #
+            # if (assignment_report["setup"]["Algorithm"] == "bfw") and (i > 2):
+            #     self.demand_weights[:(i-2)] *= beta_2
 
     def _read_path_file(self, iteration, traffic_class, origin):
+
+        iteration += 1  # aequilibrae's iterations are 1-based, but I used numpy's 0-based indexing in select_link
 
         pth = os.environ.get(ENVIRON_VAR)
         path_base_dir = os.path.join(pth, "path_files", self.assignment_results.procedure_id.values[0])
@@ -143,12 +158,6 @@ class SelectLink(object):
         assert len(self.classes) > 0, "Need at least one traffic class to run select link analysis, use set_classes"
 
         self.select_link_id = link_id
-        # look up compressed id for each class. Should be the same.
-        for c in self.classes:
-            graph = self.compressed_graph_correspondences[c.__id__]
-            self.select_link_id_compressed[c.__id__] = graph.loc[graph["link_id"] == self.link_id][
-                "__compressed_id__"
-            ].values[0]
 
         # initialise select link matrices to zero
         self._initialise_matrices()
@@ -159,14 +168,21 @@ class SelectLink(object):
         # read in compressed correspondence, depends on read_assignment results for path lookup
         self._read_compressed_graph_correspondence()
 
+        # look up compressed id for each class. Should be the same.
+        for c in self.classes:
+            graph = self.compressed_graph_correspondences[c.__id__]
+            self.select_link_id_compressed[c.__id__] = graph.loc[graph["link_id"] == self.select_link_id][
+                "__compressed_id__"
+            ].values[0]
+
         # now get weight of each iteration to weight corresponding demand.
-        # FIXME (Jan 18/4/21): this is MSA only atm, needs to be implemented
+        # FIXME (Jan 21/4/21): this is MSA and FW only atm, needs to be implemented for CFW and BFW
         self._calculate_demand_weights()
 
         # now process each iteration.
         num_centroids = self.classes[0].matrix.zones
 
-        for iteration in self.num_iters:
+        for iteration in range(self.num_iters):
             logger.info(f"Procesing iteration {iteration} for select link analysis")
             weight = self.demand_weights[iteration]
             for c in self.classes:
