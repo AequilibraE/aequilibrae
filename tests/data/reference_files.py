61ca3bed59e43e6051dbeaf975235e059273ee6b
Baris Demirdelen <barisdemirdelen@gmail.com>
2021-05-25 14:17:35 +0200

    Add tests for shortest path calculation

--- tests/data/reference_files.py
+++ tests/data/reference_files.py
@@ -7,6 +7,7 @@ data_folder = join(dirname(dirname(abspath(__file__))), 'data')
 test_network = join(dirname(dirname(abspath(__file__))), "data", "Final_Network.shp")
 test_graph = join(dirname(dirname(abspath(__file__))), "data", "test_graph.aeg")
 path_test = tempfile.gettempdir()
+triangle_graph_blocking = join(dirname(dirname(abspath(__file__))), "data", "blocking_triangle_graph_project")
 
 gtfs_folder = join(dirname(dirname(abspath(__file__))), "data/gtfs")
 gtfs_zip = join(dirname(dirname(abspath(__file__))), "data/gtfs.zip")
@@ -29,3 +30,4 @@ siouxfalls_skims = join(dirname(dirname(abspath(__file__))), "data/SiouxFalls_pr
 
 #
 no_triggers_project = join(dirname(dirname(abspath(__file__))), "data/no_triggers_project")
+
