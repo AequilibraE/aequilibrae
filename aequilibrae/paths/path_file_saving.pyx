aa5a6e11fa59785a6f8a7bb6984c0b940832547d
Jan Zill <jan.zill@veitchlister.com.au>
2021-05-29 14:43:07 +1000

    path file saving index bug fix

--- aequilibrae/paths/path_file_saving.pyx
+++ aequilibrae/paths/path_file_saving.pyx
@@ -30,8 +30,6 @@ cpdef void save_path_file(long origin_index,
     cdef vector[long long] path_data
     # could make this an ndarray and not do the conversion, we know the size of the index array is zones
     cdef vector[long long] size_of_path_arrays
-    cdef long long* temp_data
-    cdef long long* temp_data_ind
 
     cdef np.npy_intp dims[1]
     cdef np.ndarray[np.longlong_t, ndim=1] numpy_array
@@ -43,31 +41,23 @@ cpdef void save_path_file(long origin_index,
             predecessor = pred[node]
             # need to check if disconnected, also makes sure o==d is not included
             if predecessor == -1:
-                # need to store index here
-                size_of_path_arrays.push_back(<np.longlong_t> path_data.size())
+                size_of_path_arrays.push_back(<np.longlong_t> path_data.size())  # need to store index here
                 continue
             connector = conn[node]
             path_data.push_back(connector)
-
-            while predecessor >= 0:
+            while predecessor != -1:
+                connector = conn[predecessor] # connector has to be looked up BEFORE predecessor update
                 predecessor = pred[predecessor]
-                if predecessor != -1:
-                    connector = conn[predecessor]
-                    # need this to avoid ading last element. Would it be faster to resize after loop?
-                    if connector != -1:
-                        path_data.push_back(connector)
+                if (predecessor != -1) and (connector != -1):
+                    path_data.push_back(connector)
 
-            size_of_path_arrays.push_back(<np.longlong_t> path_data.size())
+        size_of_path_arrays.push_back(<np.longlong_t> path_data.size())
 
     # get a view on data underlying vector, then as numpy array. avoids copying.
     dims[0] = <np.npy_intp> (path_data.size())
     dims_ind[0] = <np.npy_intp> (size_of_path_arrays.size())
-
-    temp_data = &path_data[0]
-    temp_data_ind = &size_of_path_arrays[0]
-
-    numpy_array = np.PyArray_SimpleNewFromData(1, dims, np.NPY_LONGLONG, temp_data)
-    numpy_array_ind = np.PyArray_SimpleNewFromData(1, dims_ind, np.NPY_LONGLONG, temp_data_ind)
+    numpy_array = np.PyArray_SimpleNewFromData(1, dims, np.NPY_LONGLONG, &path_data[0])
+    numpy_array_ind = np.PyArray_SimpleNewFromData(1, dims_ind, np.NPY_LONGLONG, &size_of_path_arrays[0])
 
     if write_feather:
         feather.write_feather(pa.table({"data": numpy_array}), path_file.decode('utf-8'))
