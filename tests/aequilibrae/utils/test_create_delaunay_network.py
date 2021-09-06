f88deb40bfdfb980b88de91a585bb363170fda7b
Pedro Camargo <c@margo.co>
2021-09-06 18:18:44 +1000

    Adds delaunay line and assignment class

--- tests/aequilibrae/utils/test_create_delaunay_network.py
+++ tests/aequilibrae/utils/test_create_delaunay_network.py
@@ -3,21 +3,33 @@ from tempfile import gettempdir
 from os.path import join
 from uuid import uuid4
 from aequilibrae.utils.create_example import create_example
-from aequilibrae.utils.create_delaunay_network import create_delaunay_network
+from aequilibrae.utils.create_delaunay_network import DelaunayAnalysis
 
 
-class Test(TestCase):
+class TestDelaunayAnalysis(TestCase):
+    def setUp(self) -> None:
+        self.proj = create_example(join(gettempdir(), uuid4().hex))
+
+    def tearDown(self) -> None:
+        self.proj.close()
+
     def test_create_delaunay_network(self):
-        proj = create_example(join(gettempdir(), uuid4().hex))
+        da = DelaunayAnalysis()
+        with self.assertRaises(ValueError):
+            da.create_network('nodes')
+
+        da.create_network()
+        self.assertEqual(59, self.proj.conn.execute('select count(*) from delaunay_network').fetchone()[0])
+
+        da.create_network('network', True)
+        self.assertEqual(62, self.proj.conn.execute('select count(*) from delaunay_network').fetchone()[0])
 
         with self.assertRaises(ValueError):
-            create_delaunay_network('nodes')
+            da.create_network()
 
-        create_delaunay_network()
-
-        self.assertEqual(59, proj.conn.execute('select count(*) from delaunay_network').fetchone()[0])
-        create_delaunay_network('network', True)
-
-        self.assertEqual(62, proj.conn.execute('select count(*) from delaunay_network').fetchone()[0])
-        with self.assertRaises(ValueError):
-            create_delaunay_network()
+    def test_assign_matrix(self):
+        demand = self.proj.matrices.get_matrix("demand_omx")
+        demand.computational_view(["matrix"])
+        da = DelaunayAnalysis()
+        da.create_network()
+        da.assign_matrix(demand, 'delaunay_test')
