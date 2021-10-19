9f438278e09c875717779bfcc99bf7ba75ed1372
Pedro Camargo <c@margo.co>
2021-10-19 16:58:32 +1000

    Drops Python 3.6

--- setup.py
+++ setup.py
@@ -48,7 +48,6 @@ if __name__ == "__main__":
         license="See license.txt",
         classifiers=[
             "Programming Language :: Python",
-            "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
