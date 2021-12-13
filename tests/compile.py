450bd277ba38a0e505b4273fdb5d1ddc47443203
Pedro Camargo <veigacamargo@gmail.com>
2021-12-13 12:00:51 +1000

    Two small changes around the edges (#288)
    * Adds the obvious field vot to the modes table and creates and adds a script for compiling that may be useful during development
    * Update tests/compile.py
    Co-authored-by: Jamie Cook <jamie.cook@veitchlister.com.au>

---
+++ tests/compile.py
@@ -0,0 +1,8 @@
+from os.path import join, dirname
+import os
+import subprocess
+
+pth = join(dirname(dirname(__file__)), 'aequilibrae', 'paths')
+
+os.chdir(pth)
+subprocess.Popen(f"{sys.executable} setup_Assignment.py build_ext --inplace", shell=True, stdout=subprocess.PIPE).stdout.read()
