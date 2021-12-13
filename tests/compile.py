7463a364c8409384c6de2b56dc5eb2ba707adf62
Pedro Camargo <veigacamargo@gmail.com>
2021-12-13 12:00:23 +1000

    Update tests/compile.py
    Co-authored-by: Jamie Cook <jamie.cook@veitchlister.com.au>

--- tests/compile.py
+++ tests/compile.py
@@ -5,4 +5,4 @@ import subprocess
 pth = join(dirname(dirname(__file__)), 'aequilibrae', 'paths')
 
 os.chdir(pth)
-subprocess.Popen("python setup_Assignment.py build_ext --inplace", shell=True, stdout=subprocess.PIPE).stdout.read()
+subprocess.Popen(f"{sys.executable} setup_Assignment.py build_ext --inplace", shell=True, stdout=subprocess.PIPE).stdout.read()
