315268d2750b609cd261d35507c87cf71e036284
Pedro Camargo <c@margo.co>
2022-11-25 08:22:05 +1000

    Fixes packaging (#362)
    Co-authored-by: pveigadecamargo <pveigadecamargo@anl.gov>
    Co-authored-by: djfrancesco <pacullfrancois@gmail.com>

--- tests/compile.py
+++ tests/compile.py
@@ -7,5 +7,5 @@ pth = join(dirname(dirname(__file__)), "aequilibrae", "paths")
 
 os.chdir(pth)
 subprocess.Popen(
-    f"{sys.executable} setup_Assignment.py build_ext --inplace", shell=True, stdout=subprocess.PIPE
+    f"{sys.executable} setup_assignment.py build_ext --inplace", shell=True, stdout=subprocess.PIPE
 ).stdout.read()
