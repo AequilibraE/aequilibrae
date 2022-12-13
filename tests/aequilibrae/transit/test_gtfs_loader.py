f25e99824a6f8e8fac48f2de668e897b1683b9c6
Renata Imai <renataakemii@yahoo.co.uk>
2022-12-13 07:55:44 -0300

    adds tests

---
+++ tests/aequilibrae/transit/test_gtfs_loader.py
@@ -0,0 +1,37 @@
+from os.path import join, dirname, abspath
+from tempfile import gettempdir
+from unittest import TestCase
+from uuid import uuid4
+
+import pandas as pd
+from aequilibrae.project import Project
+
+from aequilibrae.transit.gtfs_loader import GTFSReader
+
+
+class TestGTFSReader(TestCase):
+    def setUp(self) -> None:
+        self.fldr = join(gettempdir(), uuid4().hex)
+        self.prj = Project()
+        self.prj.new(self.fldr)
+
+        self.gtfs_fldr = join(abspath(dirname("tests")), "tests/data/2020-04-01.zip")
+
+        self.cap = pd.read_csv(join(abspath(dirname("tests")), "tests/data/transit_max_speeds.csv"))
+
+        self.gtfs_loader = GTFSReader()
+
+    def test_set_feed_path(self):
+        # self.fail()
+        self.gtfs_loader.set_feed_path(self.gtfs_fldr)
+
+        with self.assertRaises(Exception):
+            self.gtfs_loader.set_feed_path(self.gtfs_fldr + "_")
+
+    def test_load_data(self):
+        df = self.cap[self.cap.city == "Austin"]
+        df.loc[df.min_distance < 100, "speed"] = 10
+        dict_speeds = {x: df for x, df in df.groupby(["mode"])}
+        self.gtfs_loader._set_maximum_speeds(dict_speeds)
+        self.gtfs_loader.set_feed_path(self.gtfs_fldr)
+        self.gtfs_loader.load_data("2020-04-01")
---
+++ tests/aequilibrae/transit/test_pattern.py
@@ -0,0 +1,45 @@
+import os
+from tempfile import gettempdir
+from unittest import TestCase
+from uuid import uuid4
+from aequilibrae.project import Project
+from aequilibrae.transit import Transit
+from aequilibrae.transit.functions.transit_connection import transit_connection
+
+from aequilibrae.transit.transit_elements import Pattern
+
+
+class TestTransitPattern(TestCase):
+    def setUp(self) -> None:
+        self.fldr = os.path.join(gettempdir(), uuid4().hex)
+        self.prj = Project()
+        self.prj.new(self.fldr)
+
+        self.gtfs_fldr = os.path.join(os.path.abspath(os.path.dirname("tests")), "tests/data/2020-04-01.zip")
+
+        agency_id = "Campo"
+        description = "CampMetro"
+        date = "2020-04-01"
+
+        self.network = transit_connection(self.fldr)
+
+        self.data = Transit(self.prj)
+        self.transit = self.data.new_gtfs(agency=agency_id, file_path=self.gtfs_fldr, description=description)
+        self.transit.load_date(date)
+
+        self.patterns = self.transit.select_patterns  # type: dict
+        self.pat = [x for x in self.patterns.values()][0]  # type: Pattern
+
+    def test_save_to_database(self):
+        self.pat.save_to_database(self.network)
+
+    # def test_best_shape(self):
+    #     shp = self.pat.best_shape()
+    #     self.assertEqual(shp, self.pat._stop_based_shape, "Returned the wrong shape")
+
+    # def test_get_error(self):
+    #     self.assertEqual(self.pat.get_error(), None, "Resulted a map-matching error when should have returned none")
+
+    # def test_map_match(self):
+    #     self.pat.map_match()
+    #     self.pat.save_to_database(self.conn)
