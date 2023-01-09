d8132441b44a1169f3a9256f80b2e7f51de26ec6
Renata Imai <renataakemii@yahoo.co.uk>
2023-01-09 08:28:55 -0300

    fixes tests

--- tests/aequilibrae/project/test_network_triggers.py
+++ tests/aequilibrae/project/test_network_triggers.py
@@ -16,8 +16,8 @@ class TestNetworkTriggers(TestCase):
         copytree(siouxfalls_project, self.proj_path)
         self.siouxfalls = Project()
         self.siouxfalls.open(self.proj_path)
-        remove_triggers(self.siouxfalls.conn, self.siouxfalls.logger)
-        add_triggers(self.siouxfalls.conn, self.siouxfalls.logger)
+        remove_triggers(self.siouxfalls.conn, self.siouxfalls.logger, table_type="network")
+        add_triggers(self.siouxfalls.conn, self.siouxfalls.logger, table_type="network")
 
     def tearDown(self) -> None:
         self.siouxfalls.close()
