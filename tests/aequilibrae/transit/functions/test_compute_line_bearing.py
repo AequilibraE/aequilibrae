b368c769d18a06f8d8882cde25531c9cd1f82b93
Pedro Camargo <c@margo.co>
2023-01-23 20:14:10 +1000

    Update tests/aequilibrae/transit/functions/test_compute_line_bearing.py
    Co-authored-by: Jamie Cook <jamie.cook@veitchlister.com.au>

--- tests/aequilibrae/transit/functions/test_compute_line_bearing.py
+++ tests/aequilibrae/transit/functions/test_compute_line_bearing.py
@@ -6,7 +6,5 @@ from aequilibrae.transit.functions.compute_line_bearing import compute_line_bear
 class TestComputeLineBearing:
     def test_compute_line_bearing(self):
         assert pytest.approx(compute_line_bearing((-1.5639, -43.7397), (-1.5818, -43.7347))) == 15.6066221
-
         assert pytest.approx(compute_line_bearing((-1.5865, -43.7183), (-1.5808, -43.7064))) == 64.40597075
-
         assert pytest.approx(compute_line_bearing((-1.5641, -43.6992), (-1.5623, -43.7177))) == 84.44276779
