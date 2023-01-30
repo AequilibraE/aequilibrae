e23260b0184ce6c737dc1de2ceedca64b4ca68aa
Pedro Camargo <c@margo.co>
2023-01-30 11:11:18 +1000

    Update aequilibrae/distribution/setup_ipf.py
    Co-authored-by: Jamie Cook <jamie.cook@veitchlister.com.au>

--- aequilibrae/distribution/setup_ipf.py
+++ aequilibrae/distribution/setup_ipf.py
@@ -9,27 +9,17 @@ except ImportError:
     from distutils.core import setup
     from distutils.extension import Extension
 
-if "WINDOWS" in platform.platform().upper():
-    ext_modules = [
-        Extension(
-            "ipf_core",
-            ["ipf_core.pyx"],
-            extra_compile_args=["/openmp"],
-            extra_link_args=["/openmp"],
-            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
-            include_dirs=[np.get_include()],
-        )
-    ]
-else:
-    ext_modules = [
-        Extension(
-            "ipf_core",
-            ["ipf_core.pyx"],
-            extra_compile_args=["-fopenmp"],  # do we want -Ofast?
-            extra_link_args=["-fopenmp"],
-            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
-            include_dirs=[np.get_include()],
-        )
-    ]
+prefix = "/" if "WINDOWS" in platform.platform().upper() else "-f"
+    
+ext_modules = [
+    Extension(
+        "ipf_core",
+        ["ipf_core.pyx"],
+        extra_compile_args=[f'{prefix}openmp'],
+        extra_link_args=[f'{prefix}openmp'],
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+        include_dirs=[np.get_include()],
+    )
+]
 
 setup(name="ipf_core", ext_modules=cythonize(ext_modules))
