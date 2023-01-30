30450de00580430df7027362edc2da240e47636a
Pedro Camargo <c@margo.co>
2023-01-31 06:58:48 +1000

    Adds parallelized version of Fratar (#378)
    * Adds parallelized version of Fratar
    * Guarantees we are not operating on disk
    * only importing the ipf_core cython module after compilation
    * Update aequilibrae/distribution/ipf_core.pyx
    ---------
    
    Co-authored-by: pveigadecamargo <pveigadecamargo@anl.gov>
    Co-authored-by: djfrancesco <pacullfrancois@gmail.com>
    Co-authored-by: Jamie Cook <jamie.cook@veitchlister.com.au>

---
+++ aequilibrae/distribution/setup_ipf.py
@@ -0,0 +1,25 @@
+import numpy as np
+import platform
+from Cython.Build import cythonize
+
+try:
+    from setuptools import setup
+    from setuptools import Extension
+except ImportError:
+    from distutils.core import setup
+    from distutils.extension import Extension
+
+prefix = "/" if "WINDOWS" in platform.platform().upper() else "-f"
+    
+ext_modules = [
+    Extension(
+        "ipf_core",
+        ["ipf_core.pyx"],
+        extra_compile_args=[f'{prefix}openmp'],
+        extra_link_args=[f'{prefix}openmp'],
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+        include_dirs=[np.get_include()],
+    )
+]
+
+setup(name="ipf_core", ext_modules=cythonize(ext_modules))
