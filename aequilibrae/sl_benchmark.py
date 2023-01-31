80055bf9ef3f6bb53eb229c8834cbbbbe6a1c14d
chrisobi02 <chrisgobrien02@gmail.com>
2023-01-31 14:22:40 +1000

    Updated benchmark to run arkansas and chicago at once, improved printouts

--- aequilibrae/sl_benchmark.py
+++ aequilibrae/sl_benchmark.py
@@ -21,25 +21,10 @@ def aequilibrae_init(proj_path: str, cost: str, cores: int = 0, ):
     proj.open(proj_path)
     # curr.execute("select st_x(geometry), st_y(geometry) from nodes")
     # geo = np.array(curr.fetchall())
-    print(proj.matrices.list())
-    #ARKANSAS SPECIFIC
     proj.network.build_graphs([cost, "capacity_ab", "capacity_ba"], ["c"])
     graph = proj.network.graphs["c"]
-    # graph.prepare_graph(graph.centroids)
-    # let's say we want to minimize the cost
-    # graph.set_graph(cost)
-    # print(graph.graph.head())
-    # print(graph.graph.columns)
-    # print(graph.compact_graph.head())
-    # And will skim the cost while we are at it
-    # graph.set_skimming(cost)
-    # raise Exception()
-    print('laoding')
     matrix = proj.matrices.get_matrix("demand_omx")
-    print(matrix)
-    print("loaded")
     matrix.computational_view()
-    print(matrix.matrix_view)
     # matrix.matrix_view = np.zeros((1790, 1790, 1))
     # matrix = AequilibraeMatrix()
     # matrix.create_empty(zones=graph.num_zones, matrix_names=["dummy"])
@@ -50,12 +35,11 @@ def aequilibrae_init(proj_path: str, cost: str, cores: int = 0, ):
     assignment.set_vdf("BPR")
     assignment.set_vdf_parameters({"alpha": 0.15, "beta": 4.0})
     # assignment.set_vdf_parameters({"alpha": "b", "beta": "power"})
-    print(graph.graph.columns)
     assignment.set_capacity_field("capacity")
     assignment.set_time_field("distance")
     assignment.max_iter = 1
     assignment.set_algorithm("msa")
-    assignment.set_cores(0)
+    # assignment.set_cores(1)
     algorithms = ["msa", "cfw", "bfw", "frank-wolfe"]
 
     # And we will allow paths to be compute going through other centroids/centroid connectors
@@ -199,14 +183,14 @@ def main():
     output_path = args["output"]
     cores = args["cores"]
     print(f"Now benchmarking {libraries} on the {args['projects']} model(s).")
-    print(f"Running with {args['iters']} iterations, {args['repeats']}",
-          f"times, for a total of {args['iters'] * args['repeats']} samples.")
+    # print(f"Running with {args['iters']} iterations, {args['repeats']}",
+    #       f"times, for a total of {args['iters'] * args['repeats']} samples.")
     # Arkansas links
-    select_links = [None, {"test": [(24, 1), (79146, 1)], "test 2": [(61, 1), (68, 1)]}]
+    # select_links = [None, {"test": [(24, 1), (79146, 1)], "test 2": [(61, 1), (68, 1)]}]
     # Chicago links
-    # select_links = [None,
-    #                 {"test": [(2, 1), (7, 1), (1, 1), (6, 1)],
-    #                 "set 2": [(1, 1), (3, 1)]}]
+    select_links = [None,
+                    {"test": [(2, 1), (7, 1), (1, 1), (6, 1)],
+                     "set 2": [(1, 1), (3, 1)]}]
 
 
     with warnings.catch_warnings():
@@ -215,51 +199,39 @@ def main():
         # proj_path: str, cost: str, select_links, cores: int = 0,
         # Benchmark time
         results = []
-        proj_series = []
-        project_name = args["projects"][0]
-        # graph, matrix, assignment, car = aequilibrae_init(f"{args['path']}/{project_name}",
-        #                                                           args["cost"], args["cores"])
-        assignment, car = arkansas()
-        for link in select_links:
-            for project_name in args["projects"]:
-                print("select links is ", link)
+
+        for project_name in args["projects"]:
+            if project_name in "chicago_sketch":
+                graph, matrix, assignment, car = aequilibrae_init(f"{args['path']}/{project_name}",
+                                                                  args["cost"], args["cores"])
+            elif project_name in "Arkansas":
+                assignment, car = arkansas()
+            else:
+                raise Exception("Model Doesn't Exist Fool")
+            assignment.set_cores(args["cores"][0])
+            for link in select_links:
                 if link is not None:
                     car.set_select_links(link)
-                print("BENCHING ")
-            # args["graph"], args["nodes"], args["geo"] = project_init(f"{args['path']}/{project_name}", args["cost"])
-            # proj_series.append(pd.DataFrame({
-            #     "num_links": [args["graph"].compact_num_links],
-            #     "num_nodes": [args["graph"].compact_num_nodes],
-            #     "num_zones": [args["graph"].num_zones],
-            #     "num_centroids": [len(args["graph"].centroids)]
-            # }, index=[project_name]))
+                print("BENCHING links: ", link)
 
-            # for core_count in (range(cores[0], cores[1] + 1) if len(cores) == 2 else cores):
-            #     args["cores"] = core_count
                 t = timeit.Timer(lambda: assignment.execute())
                 times = t.repeat(repeat=3, number=args["iters"])
-                results.append(("SL" if link is not None else "BASE", min(times)))
-
-
-            # print("-" * 30)
-        print(results)
-        print("BASE/SL", (results[1][1] - results[0][1])/results[1][1]*100)
-        #
-        # results = pd.concat(results)
-        # summary = results.groupby(["project_name", "library", "cores"]).agg(
-        #     average=("runtime", "mean"), min=("runtime", "min"), max=("runtime", "max")
-        # )
-        # time = datetime.now().strftime('%Y_%m_%d-%H_%M_%S')
-        # print(time)
-        # print(summary)
-        # results.to_csv(os.path.join(output_path, f"{time}_table.csv"))
-
-        # proj_summary = pd.concat(proj_series)
-        # proj_summary.to_csv(os.path.join(output_path, f"{time}_project_summary.csv"))
-
-
-
-
+                df = pd.DataFrame(
+                    {"Project": project_name,
+                     "Select_Link": False if link is None else True,
+                     "Minimum_Runtime": [min(times)]})
+                results.append(df)
+        ratios = []
+        final = pd.concat(results)
+        for project_name in args["projects"]:
+            no_sl = final.loc[(final["Project"] == project_name) & (final["Select_Link"] == False)]["Minimum_Runtime"].values[0]
+            sl = final.loc[(final["Project"] == project_name) & (final["Select_Link"] == True)]["Minimum_Runtime"].values[0]
+            df = pd.DataFrame({"Project": project_name, "Runtime_%_Increase": [(sl-no_sl)/no_sl*100]})
+            ratios.append(df)
+        final_ratio = pd.concat(ratios)
+        print(final)
+        print("\n")
+        print(final_ratio)
 
 if __name__ == "__main__":
     main()
