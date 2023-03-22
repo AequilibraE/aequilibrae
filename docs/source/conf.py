21e32c171e7ba6049b3a083181eece1842dd0c25
james.cook <jimi.cook@gmail.com>
2023-03-22 02:13:29 -0500

    .

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,6 +22,11 @@ project_dir = Path(__file__).parent.parent
 if str(project_dir) not in sys.path:
     sys.path.append(str(project_dir))
 
+# Sometimes this file is exec'd directly from sphinx code...
+project_dir = os.path.abspath("../..")
+if str(project_dir) not in sys.path:
+    sys.path.insert(0, project_dir)
+
 from __version__ import release_version
 
 
