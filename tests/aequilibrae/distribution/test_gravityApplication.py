cde31faa34edd13001624be7a73e3e1cfb3e35de
pveigadecamargo <pveigadecamargo@anl.gov>
2023-03-27 13:48:25 +1000

    already tests without a project open

--- tests/aequilibrae/distribution/test_gravityApplication.py
+++ tests/aequilibrae/distribution/test_gravityApplication.py
@@ -78,13 +78,4 @@ class TestGravityApplication(TestCase):
             distributed_matrix.apply()
 
             if distributed_matrix.gap > distributed_matrix.parameters["convergence level"]:
-                self.fail("Gravity application did not converge for model " + model_name)
-
-        # Tests without a project open
-        for model_name, model_obj in models:
-            args["model"] = model_obj
-            distributed_matrix = GravityApplication(**args)
-            distributed_matrix.apply()
-
-            if distributed_matrix.gap > distributed_matrix.parameters["convergence level"]:
-                self.fail("Gravity application did not converge for model " + model_name)
+                self.fail(f"Gravity application did not converge for model {model_name}")
