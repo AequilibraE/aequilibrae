86bdfb61d72bc0527074beaf8f3e11a694fe32d9
pveigadecamargo <pveigadecamargo@anl.gov>
2023-04-04 18:15:03 +1000

    introduces data model documentation

--- docs/website/check_documentation_versions.py
+++ docs/website/check_documentation_versions.py
@@ -6,7 +6,7 @@ if npth not in sys.path:
     sys.path.append(npth)
     print(npth)
 
-with open("../../__version__.py") as f:
+with open("../__version__.py") as f:
     exec(f.read())
 
 # We check if the reference to all existing versions were added by checking
