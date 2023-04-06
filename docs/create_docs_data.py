ac84b7e7b67731b3da554591be8a545452579192
pveigadecamargo <pveigadecamargo@anl.gov>
2023-04-06 20:48:03 +1000

    documentation and testing of it

--- docs/create_docs_data.py
+++ docs/create_docs_data.py
@@ -1,3 +1,10 @@
+import sys
+from pathlib import Path
+
+project_dir = Path(__file__).parent.parent
+if str(project_dir) not in sys.path:
+    sys.path.append(str(project_dir))
+
 from aequilibrae.utils.create_example import create_example
 
 project = create_example("/tmp/test_project")
