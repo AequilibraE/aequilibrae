16772d4bb5f9c82e54692f935da03e9468365f31
djfrancesco <pacullfrancois@gmail.com>
2023-05-10 11:30:16 +0200

    added the COO to CSC sparse format converter

--- aequilibrae/paths/hyperpath.pyx
+++ aequilibrae/paths/hyperpath.pyx
@@ -37,6 +37,88 @@ A_VERY_SMALL_TIME_INTERVAL_PY = A_VERY_SMALL_TIME_INTERVAL
 include 'pq_4ary_heap.pyx'  # priority queue
 
 
+cdef void _coo_tocsc_uint32(
+    cnp.uint32_t [::1] Ai,
+    cnp.uint32_t [::1] Aj,
+    cnp.uint32_t [::1] Ax,   
+    cnp.uint32_t [::1] Bp,
+    cnp.uint32_t [::1] Bi,
+    cnp.uint32_t [::1] Bx) nogil:
+
+    cdef:
+        size_t i, col, dest
+        size_t n_vert = <size_t>(Bp.shape[0] - 1)
+        size_t n_edge = <size_t>Bi.shape[0]
+        cnp.uint32_t temp, cumsum, last
+
+    for i in range(n_edge):
+        Bp[<size_t>Aj[i]] += 1
+
+    cumsum = 0
+    for i in range(n_vert):
+        temp = Bp[i]
+        Bp[i] = cumsum
+        cumsum += temp
+    Bp[<size_t>n_vert] = <cnp.uint32_t>n_edge 
+
+    for i in range(n_edge):
+        col  = <size_t>Aj[i]
+        dest = <size_t>Bp[col]
+        Bi[dest] = Ai[i]
+        Bx[dest] = Ax[i]
+        Bp[col] += 1
+
+    last = 0
+    for i in range(n_vert + 1):
+        temp = Bp[i]
+        Bp[i] = last
+        last = temp
+
+
+cpdef convert_graph_to_csc_uint32(edges, tail, head, data, vertex_count):
+    """
+    Convert an edge dataframe in COO format into CSC format.
+
+    The data vector is of uint32 type.
+
+    Parameters
+    ----------
+    edges : pandas.core.frame.DataFrame
+        The edges dataframe.
+    tail : str
+        The column name in the edges dataframe for the tail vertex index.
+    head : str
+        The column name in the edges dataframe for the head vertex index.
+    data : str
+        The column name in the edges dataframe for the int edge attribute.
+    vertex_count : int
+        The vertex count in the given network edges.
+
+    Returns
+    -------
+    tuple[numpy.ndarray, numpy.ndarray, numpy.ndarray]
+    """
+
+    rs_indptr = np.zeros(
+        vertex_count + 1, dtype=np.uint32
+    )  # make sure it is filled with zeros
+    edge_count = len(edges)
+    rs_indices = np.empty(edge_count, dtype=np.uint32)
+    rs_data = np.empty(edge_count, dtype=np.uint32)
+
+    _coo_tocsc_uint32(
+        edges[tail].values.astype(np.uint32),
+        edges[head].values.astype(np.uint32),
+        edges[data].values.astype(np.uint32),
+        rs_indptr,
+        rs_indices,
+        rs_data,
+    )
+
+    return rs_indptr, rs_indices, rs_data
+
+
+
 cpdef void compute_SF_in(
     cnp.uint32_t[::1] csc_indptr,
     cnp.uint32_t[::1] csc_edge_idx,
