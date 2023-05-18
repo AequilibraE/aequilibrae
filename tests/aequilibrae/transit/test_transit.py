7b93cf992bccfd0c669f7ee28f129376f7a854c3
Renata Imai <53949163+r-akemii@users.noreply.github.com>
2023-05-17 23:18:33 -0300

    fixes agencies table (#414)
    * fixes agencies IDs
    
    ---------
    
    Co-authored-by: pveigadecamargo <pveigadecamargo@anl.gov>

--- tests/aequilibrae/transit/test_transit.py
+++ tests/aequilibrae/transit/test_transit.py
@@ -1,13 +1,43 @@
 from os.path import isfile, join
 
+from aequilibrae.project.database_connection import database_connection
+from aequilibrae.transit.constants import Constants
+
 
 def test_new_gtfs_builder(create_gtfs_project, create_path):
+    c = Constants()
+    c.agencies["agencies"] = 0
+
+    conn = database_connection("transit")
+    existing = conn.execute("SELECT COALESCE(MAX(DISTINCT(agency_id)), 0) FROM agencies;").fetchone()[0]
+
     transit = create_gtfs_project.new_gtfs_builder(
-        agency="LISERCO, LISANCO, LINCOSUR",
+        agency="Agency_1",
+        day="2016-04-13",
         file_path=join(create_path, "gtfs_coquimbo.zip"),
     )
 
     assert str(type(transit)) == "<class 'aequilibrae.transit.lib_gtfs.GTFSRouteSystemBuilder'>"
+
+    transit2 = create_gtfs_project.new_gtfs_builder(
+        agency="Agency_2",
+        day="2016-07-19",
+        file_path=join(create_path, "gtfs_coquimbo.zip"),
+    )
+
+    transit.save_to_disk()
+    transit2.save_to_disk()
+
+    assert conn.execute("SELECT MAX(DISTINCT(agency_id)) FROM agencies;").fetchone()[0] == existing + 2
+
+    transit3 = create_gtfs_project.new_gtfs_builder(
+        agency="Agency_3",
+        day="2016-07-19",
+        file_path=join(create_path, "gtfs_coquimbo.zip"),
+    )
+
+    transit3.save_to_disk()
+    assert conn.execute("SELECT MAX(DISTINCT(agency_id)) FROM agencies;").fetchone()[0] == existing + 3
 
 
 def test___create_transit_database(create_gtfs_project):
