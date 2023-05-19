8b4a53c255755d5bde7e9d6b50d4028ddb2907a6
djfrancesco <pacullfrancois@gmail.com>
2023-05-19 16:14:04 +0200

    assignment loop

--- aequilibrae/paths/public_transport.pyx
+++ aequilibrae/paths/public_transport.pyx
@@ -6,15 +6,7 @@ include 'hyperpath.pyx'
 
 
 class HyperpathGenerating:
-    def __init__(
-        self,
-        edges,
-        tail="tail",
-        head="head",
-        trav_time="trav_time",
-        freq="freq",
-        check_edges=False
-    ):
+    def __init__(self, edges, tail="tail", head="head", trav_time="trav_time", freq="freq", check_edges=False):
         # load the edges
         if check_edges:
             self._check_edges(edges, tail, head, trav_time, freq)
@@ -30,12 +22,8 @@ class HyperpathGenerating:
             A_VERY_SMALL_TIME_INTERVAL_PY,
             self._edges[trav_time],
         )
-        self._edges[freq] = np.where(
-            self._edges[freq] > INF_FREQ_PY, INF_FREQ_PY, self._edges[freq]
-        )
-        self._edges[freq] = np.where(
-            self._edges[freq] < MIN_FREQ_PY, MIN_FREQ_PY, self._edges[freq]
-        )
+        self._edges[freq] = np.where(self._edges[freq] > INF_FREQ_PY, INF_FREQ_PY, self._edges[freq])
+        self._edges[freq] = np.where(self._edges[freq] < MIN_FREQ_PY, MIN_FREQ_PY, self._edges[freq])
 
         # create an edge index column
         self._edges = self._edges.reset_index(drop=True)
@@ -44,9 +32,7 @@ class HyperpathGenerating:
 
         # convert to CSC format
         self.vertex_count = self._edges[[tail, head]].max().max() + 1
-        rs_indptr, _, rs_data = convert_graph_to_csc_uint32(
-            self._edges, tail, head, data_col, self.vertex_count
-        )
+        rs_indptr, _, rs_data = convert_graph_to_csc_uint32(self._edges, tail, head, data_col, self.vertex_count)
         self._indptr = rs_indptr.astype(np.uint32)
         self._edge_idx = rs_data.astype(np.uint32)
 
@@ -110,9 +96,7 @@ class HyperpathGenerating:
 
         for col in [tail, head, trav_time, freq]:
             if col not in edges:
-                raise KeyError(
-                    f"edge column '{col}' not found in graph edges dataframe"
-                )
+                raise KeyError(f"edge column '{col}' not found in graph edges dataframe")
 
         if edges[[tail, head, trav_time, freq]].isna().any().any():
             raise ValueError(
@@ -134,3 +118,93 @@ class HyperpathGenerating:
 
             if edges[col].min() < 0.0:
                 raise ValueError(f"column '{col}' should be nonnegative")
+
+    def assign(
+        self,
+        demand,
+        origin_column="orig_vert_idx",
+        destination_column="dest_vert_idx",
+        demand_column="demand",
+        check_demand=False,
+    ):
+        # check the input demand paramater
+        if check_demand:
+            self._check_demand(demand, origin_column, destination_column, demand_column)
+
+        # initialize the column storing the resulting edge volumes
+        self._edges["volume"] = 0.0
+
+        # travel time is computed but not saved into an array in the following
+        self.u_i_vec = None
+
+        o_vert_ids = demand[origin_column].values
+        d_vert_ids = demand[destination_column].values
+        demand_vls = demand[demand_column].values
+
+        # get the list of all destinations
+        destination_vertex_indices = np.unique(d_vert_ids)
+
+        # loop on destination vertices
+        for destination_vertex_index in destination_vertex_indices:
+            demand_indices = np.where(d_vert_ids == destination_vertex_index)[0]
+
+            # list of origin vertices
+            demand_origins = o_vert_ids[demand_indices]
+            demand_origins = demand_origins.astype(np.uint32)
+
+            # list of demand values
+            demand_values = demand_vls[demand_indices]
+            demand_values = demand_values.astype(DATATYPE_PY)
+
+            # allocation/initialization of the vertex least travel time
+            u_i_vec = DATATYPE_INF_PY * np.ones(self.vertex_count, dtype=DATATYPE_PY)
+
+            # initialization of the edge volume vector
+            edge_volume = np.zeros_like(self._edges["volume"])
+
+            # S&F
+            compute_SF_in(
+                self._indptr,
+                self._edge_idx,
+                self._trav_time,
+                self._freq,
+                self._tail,
+                self._head,
+                demand_origins,  # source vertex indices
+                demand_values,
+                edge_volume,
+                u_i_vec,
+                self.vertex_count,
+                destination_vertex_index,
+            )
+            self._edges["volume"] += edge_volume
+
+    def _check_demand(self, demand, origin_column, destination_column, demand_column):
+        if type(demand) != pd.core.frame.DataFrame:
+            raise TypeError("demand should be a pandas DataFrame")
+
+        for col in [origin_column, destination_column, demand_column]:
+            if col not in demand:
+                raise KeyError(f"demand column '{col}' not found in demand dataframe")
+
+        if demand[[origin_column, destination_column, demand_column]].isna().any().any():
+            raise ValueError(
+                " ".join(
+                    [
+                        f"demand[[{origin_column}, {destination_column}, {demand_column}]] ",
+                        "should not have any missing value",
+                    ]
+                )
+            )
+
+        for col in [origin_column, destination_column]:
+            if not pd.api.types.is_integer_dtype(demand[col].dtype):
+                raise TypeError(f"column '{col}' should be of integer type")
+
+        col = demand_column
+
+        if not pd.api.types.is_numeric_dtype(demand[col].dtype):
+            raise TypeError(f"column '{col}' should be of numeric type")
+
+        if demand[col].min() < 0.0:
+            raise ValueError(f"column '{col}' should be nonnegative")
