677d836d50f914e3fad133a7d6ea9a97d5528dc2
djfrancesco <pacullfrancois@gmail.com>
2023-06-14 21:24:32 +0200

    renamed a column

--- aequilibrae/transit/transit_graph_builder.py
+++ aequilibrae/transit/transit_graph_builder.py
@@ -12,7 +12,7 @@ class SF_graph_builder:
         self.conn = conn  # sqlite connection
 
     def create_stop_vertices(self):
-        df_stop_vertices = pd.read_sql(sql="SELECT stop_id, ST_AsText(geometry) wkt FROM stops", con=self.conn)
+        df_stop_vertices = pd.read_sql(sql="SELECT stop_id, ST_AsText(geometry) coord FROM stops", con=self.conn)
         df_stop_vertices["line_id"] = None
         df_stop_vertices["taz_id"] = None
         df_stop_vertices["line_seg_idx"] = np.int32(-1)
