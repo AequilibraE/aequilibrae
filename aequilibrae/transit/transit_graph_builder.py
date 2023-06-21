097835c8e84940500350accb803a776529c6ed52
djfrancesco <pacullfrancois@gmail.com>
2023-06-21 12:10:49 +0200

    added the class global structure

--- aequilibrae/transit/transit_graph_builder.py
+++ aequilibrae/transit/transit_graph_builder.py
@@ -20,3 +20,80 @@ class SF_graph_builder:
         df_stop_vertices["vert_idx"] = np.arange(len(df_stop_vertices))
 
         return df_stop_vertices
+
+    def create_boarding_vertices(self):
+        pass
+
+    def create_alighting_vertices(self):
+        pass
+
+    def create_od_vertices(self):
+        pass
+
+    def create_vertices(self):
+        """Graph vertices creation as a dataframe.
+
+        Vertices have the following attributes:
+            - type (either 'stop', 'boarding', 'alighting', 'od', 'walking' or 'fictitious'): str
+            - coord (WKT): str
+            - stop_id (only applies to 'stop', 'boarding' and 'alighting' vertices): str
+            - line_id (only applies to 'boarding' and 'alighting' vertices): str
+            - line_seg_idx (only applies to 'boarding' and 'alighting' vertices): int
+            - taz_id (only applies to 'od' nodes): str
+
+        Note that line_seg_idx is using a 1-based indexing.
+
+        """
+
+        df_stop_vertices = self.create_stop_vertices()
+        # df_boarding_vertices = self.create_boarding_vertices()
+        # df_alighting_vertices = self.create_alighting_vertices()
+        # df_od_vertices = self.create_od_vertices()
+        # Create ids
+
+    def create_on_board_edges(self):
+        pass
+
+    def create_boarding_edges(self):
+        pass
+
+    def create_alighting_edges(self):
+        pass
+
+    def create_dwell_edges(self):
+        pass
+
+    def create_connector_edges(self):
+        pass
+
+    def create_inner_stop_transfer_edges(self):
+        pass
+
+    def create_outer_stop_transfer_edges(self):
+        pass
+
+    def create_transfer_edges(self):
+        df_inner_stop_transfer_edges = self.create_inner_stop_transfer_edges()
+        df_outer_stop_transfer_edges = self.create_outer_stop_transfer_edges()
+        pass
+
+    def create_walking_edges(self):
+        pass
+
+    def __create_edges(self):
+        """Graph edges creation as a dataframe.
+
+        Edges have the following attributes:
+            - type (either 'on-board', 'boarding', 'alighting', 'dwell', 'transfer', 'connector' or 'walking'): str
+            - line_id (only applies to 'on-board', 'boarding', 'alighting' and 'dwell' edges): str
+            - stop_id: str
+            - line_seg_idx (only applies to 'on-board', 'boarding' and 'alighting' edges): int
+            - tail_vert_idx: int
+            - head_vert_idx: int
+            - trav_time (edge travel time): float
+            - freq (frequency): float
+            - o_line_id: str
+            - d_line_id: str
+            - transfer_id: str
+
+        """
