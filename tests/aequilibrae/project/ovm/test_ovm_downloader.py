a9ad0f614eb5582ae8e10812efe1e06b62c0c308
17Pens <penny.farrell.04@gmail.com>
2023-12-14 16:13:57 +1000

    test_download in test_ovm_downloader.py updated

--- tests/aequilibrae/project/ovm/test_ovm_downloader.py
+++ tests/aequilibrae/project/ovm/test_ovm_downloader.py
@@ -13,7 +13,7 @@ pyqt = spec is not None
 
 test_data_dir = Path(__file__)
 
-data_dir = Path(__file__).parent.parent.parent.parent / "data" /"overture"/"theme=transportation"/"type=segment"
+data_dir = Path(__file__).parent.parent.parent.parent / "data" /"overture"/"theme=transportation"
 print(data_dir)
 
 class TestOVMDownloader(TestCase):
@@ -24,41 +24,42 @@ class TestOVMDownloader(TestCase):
     def test_download(self):
         # if not self.should_do_work():
         #     return
-       
         o = OVMDownloader(["car"], self.pth)
         with tempfile.TemporaryDirectory() as output_dir:
-            output_dir = Path(output_dir)
-            o.downloadTransportation([148.72095, -20.26910, 148.72405, -20.26711], data_source=data_dir, output_dir=output_dir)
+            list_element = 0
+            for t in ['segment', 'connector']:
+                output_dir = Path(output_dir) 
+                bbo =[148.71641, -20.27082, 148.71861, -20.27001]
+                box1 = [148.713909, -20.272261, 148.7206475,-20.2702697]
+                woolworths_parkinglot = [148.718, -20.27049, 148.71889, -20.27006]
+                list_gdf = o.downloadTransportation(bbox=box1, data_source=data_dir, output_dir=output_dir)
+                                        
+                expected_file = output_dir / f"type={t}" / f"transportation_data_{t}.parquet"
+                assert expected_file.exists()
 
-            expected_file = output_dir / "transportation_data_segment.parquet"
-            assert expected_file.exists()
+                
+                # gdf = gpd.read_parquet(expected_file)[list_element]
+                gdf = list_gdf[list_element]
+                assert gdf.shape[0] > 0
 
-            
-            gdf = gpd.read_parquet(expected_file)
-            assert gdf.shape[0] > 0
+                assert 'link_type' in gdf.columns
+                
+                
+                assert ['unknown', 'secondary', 'residential', 'parkingAisle', None] == list(list_gdf[0]['link_type'].unique())
 
-            assert 'road' in gdf.columns
-            
+                list_element+=1
+
 
         # if o.parquet:
         #     self.fail("It found links in the middle of the ocean")
 
-    def test_do_work2(self):
+    def test_format(self):
         # if not self.should_do_work():
         #     return
 
         # LITTLE PLACE IN THE MIDDLE OF THE Grand Canyon North Rim
         o = OVMDownloader(["car"], self.pth)
-        o.load_links()
-
-        if "elements" not in o.json[0]:
-            self.fail
-
-        if len(o.json[0]["elements"]) > 1000:
-            self.fail("It found too many elements in the middle of the Grand Canyon")
-
-        if len(o.json[0]["elements"]) < 10:
-            self.fail("It found too few elements in the middle of the Grand Canyon")
+        # o.load_links()
 
     def should_do_work(self):
         thresh = 1.01 if os.environ.get("GITHUB_WORKFLOW", "ERROR") == "Code coverage" else 0.02
