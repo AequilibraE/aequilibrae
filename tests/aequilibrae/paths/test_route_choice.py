614b77bd19efb64e005e832611fadba1e0db027a
Jake-Moss <jake.moss@uqconnect.edu.au>
2024-01-16 16:00:51 +1000

    Scratch testing on Arkansas

--- tests/aequilibrae/paths/test_route_choice.py
+++ tests/aequilibrae/paths/test_route_choice.py
@@ -20,12 +20,12 @@ class TestRouteChoice(TestCase):
         # os.mkdir(proj_path)
         # zipfile.ZipFile(join(dirname(siouxfalls_project), "sioux_falls_single_class.zip")).extractall(proj_path)
 
-        proj_path = "/home/jake/Software/aequilibrae_performance_tests/models/chicago_sketch/"
+        proj_path = "/home/jake/Software/aequilibrae_performance_tests/models/Arkansas/"
         self.project = Project()
         self.project.open(proj_path)
-        self.project.network.build_graphs()
+        self.project.network.build_graphs(fields=["distance"], modes=["c"])
         self.graph = self.project.network.graphs["c"]  # type: Graph
-        self.graph.set_graph("free_flow_time")
+        self.graph.set_graph("distance")
         self.graph.set_blocked_centroid_flows(False)
 
     def tearDown(self) -> None:
@@ -34,11 +34,19 @@ class TestRouteChoice(TestCase):
     def test_route_choice(self):
         rc = RouteChoice(self.graph)
 
-        # breakpoint()
-        results = rc.run(1, 20, max_routes=5000, max_depth=6)
+        results = rc.run(220591, 352, max_routes=5000, max_depth=0)
         # print(*results, sep="\n")
         print(len(results), len(set(results)))
         self.assertEqual(len(results), len(set(results)))
+
+        # import shapely
+        # links = self.project.network.links.data.set_index("link_id")
+        # df = []
+        # for route in results:
+        #     df.append((route, shapely.MultiLineString(links.loc[self.graph.graph[self.graph.graph.__compressed_id__.isin(route)].link_id].geometry.to_list()).wkt))
+
+        # df = pd.DataFrame(df, columns=["route", "geometry"])
+        # df.to_csv("test1.csv")
 
         # breakpoint()
 
