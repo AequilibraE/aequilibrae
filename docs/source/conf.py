17317d6e8f78e73243df296816c4f8a55e95ee0f
Renata Imai <renataakemii@yahoo.co.uk>
2024-01-19 11:49:59 -0300

    .

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -121,7 +121,7 @@ html_theme = "pydata_sphinx_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = ['_static', '_static/.htaccess', "_static/switcher.json"]
 
 html_theme_options = {
     "navbar_center": ["navbar-nav"],
