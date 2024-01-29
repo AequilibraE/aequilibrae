0c7645370d4fad35b3771d16106184ff80ab3217
17Pens <penny.farrell.04@gmail.com>
2024-01-30 09:13:49 +1000

    broke all tests splitting code

--- tests/aequilibrae/project/ovm/test_ovm_downloader.py
+++ tests/aequilibrae/project/ovm/test_ovm_downloader.py
@@ -3,6 +3,9 @@ import tempfile
 from pathlib import Path
 from tempfile import gettempdir, mkdtemp
 from unittest import TestCase
+from uuid import uuid4
+from os.path import join
+from aequilibrae import Project
 import os
 import geopandas as gpd
 from aequilibrae.project.network.ovm_downloader import OVMDownloader
@@ -13,28 +16,34 @@ pyqt = spec is not None
 
 test_data_dir = Path(__file__)
 
-data_dir = Path(__file__).parent.parent.parent.parent / "data" /"overture"/"theme=transportation"
-print(data_dir)
+data_dir = Path(__file__).parent.parent.parent.parent / "data" / "overture" / "theme=transportation"
+
 
 class TestOVMDownloader(TestCase):
     def setUp(self) -> None:
         os.environ["PATH"] = os.path.join(gettempdir(), "temp_data") + ";" + os.environ["PATH"]
         self.pth = Path(mkdtemp(prefix="aequilibrae"))
+        # fldr = join(gettempdir(), uuid4().hex)
+        # self.project = Project()
+        # self.project.new(fldr)
 
     def test_download(self):
         # if not self.should_do_work():
         #     return
-        o = OVMDownloader(["car"], self.pth)
+        print(self.pth)
+        o = OVMDownloader(["car"], self.pth, project=None)
         with tempfile.TemporaryDirectory() as output_dir:
             list_element = 0
             for t in ['segment', 'connector']:
-                output_dir = Path(output_dir) 
+                output_dir = Path(output_dir)
                 bbo =[148.71641, -20.27082, 148.71861, -20.27001]
                 box1 = [148.713909, -20.272261, 148.7206475,-20.2702697]
                 woolworths_parkinglot = [148.718, -20.27049, 148.71889, -20.27006]
+                print(data_dir)
+                print(output_dir)
                 list_gdf = o.downloadTransportation(bbox=box1, data_source=data_dir, output_dir=output_dir)
                                         
-                expected_file = output_dir / f"type={t}" / f"transportation_data_{t}.parquet"
+                expected_file = output_dir / f'theme=transportation' / f"type={t}" / f"transportation_data_{t}.parquet"
                 assert expected_file.exists()
 
                 gdf = list_gdf[list_element]
