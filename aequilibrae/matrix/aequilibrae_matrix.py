f13f4c5da7eff0b7cf2de25cc1abc49ad48002d5
pveigadecamargo <pveigadecamargo@anl.gov>
2024-01-29 15:52:39 +1000

    new matrix API

--- aequilibrae/matrix/aequilibrae_matrix.py
+++ aequilibrae/matrix/aequilibrae_matrix.py
@@ -90,7 +90,7 @@ class AequilibraeMatrix(object):
         self.omx_file = None  # type: omx.File
         self.__version__ = VERSION  # Writes file version
 
-    def save(self, names=()) -> None:
+    def save(self, names=(), file_name=None) -> None:
         """Saves matrix data back to file.
 
         If working with AEM file, it flushes data to disk. If working with OMX, requires new names.
@@ -98,6 +98,10 @@ class AequilibraeMatrix(object):
         :Arguments:
             **names** (:obj:`tuple(str)`, `Optional`): New names for the matrices. Required if working with OMX files
         """
+        if file_name is not None:
+            cores = names if len(names) else self.names
+            self.__save_as(file_name, cores)
+            return
 
         if not self.__omx:
             self.__flush(self.matrices)
@@ -121,6 +125,38 @@ class AequilibraeMatrix(object):
 
         self.names = self.omx_file.list_matrices()
         self.computational_view(names)
+
+    def __save_as(self, file_name: str, cores: List[str]):
+
+        if ".aem" in file_name:
+            mat = AequilibraeMatrix()
+            args = {
+                "zones": self.zones,
+                "matrix_names": cores,
+                "index_names": self.index_names,
+                "memory_only": False,
+                "file_name": file_name,
+            }
+            mat.create_empty(**args)
+            mat.indices[:, :] = self.indices[:, :]
+            for core in cores:
+                mat.matrix[core][:, :] = self.matrix[core][:, :]
+            mat.name = self.name
+            mat.description = self.description
+            mat.close()
+            del mat
+
+        elif ".omx" in file_name:
+            omx_mat = omx.open_file(file_name, "w")
+            for core in cores:
+                omx_mat[core] = self.matrix[core]
+
+            for index in self.index_names:
+                omx_mat.create_mapping(index, self.indices[index])
+
+            omx_mat.attrs.name = self.name
+            omx_mat.attrs.description = self.description
+            omx_mat.close()
 
     def create_empty(
         self,
@@ -570,9 +606,9 @@ class AequilibraeMatrix(object):
             np.memmap(self.file_path, dtype="uint8", offset=17, mode="r+", shape=1)[0] = data_size
 
             # matrix name
-            np.memmap(self.file_path, dtype="S" + str(MATRIX_NAME_MAX_LENGTH), offset=18, mode="r+", shape=1)[
-                0
-            ] = self.name
+            np.memmap(self.file_path, dtype="S" + str(MATRIX_NAME_MAX_LENGTH), offset=18, mode="r+", shape=1)[0] = (
+                self.name
+            )
 
             # matrix description
             offset = 18 + MATRIX_NAME_MAX_LENGTH
@@ -1095,9 +1131,9 @@ class AequilibraeMatrix(object):
             if len(str(matrix_name)) > MATRIX_NAME_MAX_LENGTH:
                 matrix_name = str(matrix_name)[0:MATRIX_NAME_MAX_LENGTH]
 
-            np.memmap(self.file_path, dtype="S" + str(MATRIX_NAME_MAX_LENGTH), offset=18, mode="r+", shape=1)[
-                0
-            ] = matrix_name
+            np.memmap(self.file_path, dtype="S" + str(MATRIX_NAME_MAX_LENGTH), offset=18, mode="r+", shape=1)[0] = (
+                matrix_name
+            )
 
     def setDescription(self, matrix_description: str):
         """
--- aequilibrae/paths/results/__init__.py
+++ aequilibrae/paths/results/__init__.py
@@ -6,6 +6,7 @@ path computation related code
 STILL NEED TO ADD SOME EXPLANATIONS HERE
 
 """
+
 __author__ = "Pedro Camargo ($Author: Pedro Camargo $)"
 __version__ = "1.0"
 __revision__ = "$Revision: 1 $"
