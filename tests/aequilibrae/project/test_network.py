090480be977ec8f51250a07a8e7c8fb4fac685df
Jake-Moss <jake.moss@uqconnect.edu.au>
2024-02-02 09:53:50 +1000

    Remove indentation and if block

--- tests/aequilibrae/project/test_network.py
+++ tests/aequilibrae/project/test_network.py
@@ -24,39 +24,36 @@ class TestNetwork(TestCase):
         self.siouxfalls.close()
 
     def test_create_from_osm(self):
-        thresh = 0.05
         if os.environ.get("GITHUB_WORKFLOW", "ERROR") == "Code coverage":
-            thresh = 1.01
-
-        if 0 < thresh:
-            self.siouxfalls.close()
-            self.project = Project()
-            self.project.new(self.proj_path2)
-            self.project.network.create_from_osm(model_area=box(-112.185, 36.59, -112.179, 36.60))
-            curr = self.project.conn.cursor()
-
-            curr.execute("""select count(*) from links""")
-            lks = curr.fetchone()[0]
-
-            curr.execute("""select count(distinct osm_id) from links""")
-            osmids = curr.fetchone()[0]
-
-            if osmids == 0:
-                warn("COULD NOT RETRIEVE DATA FROM OSM")
-                return
-
-            if osmids >= lks:
-                self.fail("OSM links not broken down properly")
-
-            curr.execute("""select count(*) from nodes""")
-            nds = curr.fetchone()[0]
-
-            if lks > nds:
-                self.fail("We imported more links than nodes. Something wrong here")
-            self.project.close()
-            self.siouxfalls.open(self.proj_path)
-        else:
             print("Skipped check to not load OSM servers")
+            return
+
+        self.siouxfalls.close()
+        self.project = Project()
+        self.project.new(self.proj_path2)
+        self.project.network.create_from_osm(model_area=box(-112.185, 36.59, -112.179, 36.60))
+        curr = self.project.conn.cursor()
+
+        curr.execute("""select count(*) from links""")
+        lks = curr.fetchone()[0]
+
+        curr.execute("""select count(distinct osm_id) from links""")
+        osmids = curr.fetchone()[0]
+
+        if osmids == 0:
+            warn("COULD NOT RETRIEVE DATA FROM OSM")
+            return
+
+        if osmids >= lks:
+            self.fail("OSM links not broken down properly")
+
+        curr.execute("""select count(*) from nodes""")
+        nds = curr.fetchone()[0]
+
+        if lks > nds:
+            self.fail("We imported more links than nodes. Something wrong here")
+        self.project.close()
+        self.siouxfalls.open(self.proj_path)
 
     def test_count_centroids(self):
         items = self.siouxfalls.network.count_centroids()
