cc7fd1cf37505b8a3af180b85a4203de6b2b6b52
mitsudz <MittunS2003@gmail.com>
2024-02-02 06:20:27 +0000

    Reworked results to have dataframe for links and for iterations

--- aequilibrae/paths/odme_/results.py
+++ aequilibrae/paths/odme_/results.py
@@ -13,38 +13,42 @@ class ODMEResults(object):
     """ Results and statistics of an ODME procedure """
     # Columns for various dataframes:
 
-    # THIS ONE GETS WRITTEN TO THE procedure_report
-    DATA_COLS = ["Outer Loop #", "Inner Loop #", "Total Iteration #", "Total Run Time (s)",
-        "Loop Time (s)", "Convergence", "Inner Convergence", "class", "obs_volume", "assign_volume"
-        'mean_factor', 'median_factor', 'std_deviation_factor', 'variance_factor', 'min_factor', 'max_factor']
-        
+    # This one get written to the procedure_report
+    ITERATION_COLS = ["class", "Outer Loop #", "Inner Loop #", "Total Iteration #",
+        "Total Run Time (s)", "Loop Time (s)", "Convergence", "Inner Convergence",
+        "Flow Objective", "Reg Objective",
+        'mean_factor', 'median_factor', 'std_deviation_factor',
+        'variance_factor', 'min_factor', 'max_factor']
+
     # This only for debugging
-    LINK_COLS = ["Outer Loop #", "Inner Loop #", "link_id", "direction", "class",
-                 "obs_volume", "assign_volume", "Assigned - Observed", "Flow Objective", "Reg Objective"]
+    LINK_COLS = ["class", "link_id", "direction", "Outer Loop #", "Inner Loop #",
+                 "obs_volume", "assign_volume", "Assigned - Observed"]
 
     # For logging different iterations:
     INNER, OUTER, FINAL_LOG = 0, 1, 2
 
-    # FIGURE OUT HOW TO DO TYPEHINT PROPERLY
-    def __init__(self, odme) -> None:
+    def __init__(self, odme: 'ODME') -> None:
         """
         Initialises necessary fields from odme object in order to generate
         statistics and results.
         """
-        # Dataframes to log statistical information:
-        self.statistics = []
-
-        # Stats on scaling matrices
-        self.factor_stats = pd.DataFrame(columns=self.FACTOR_COLS)
-
-        # Iteration number data:
-        self.total_iter, self.total_inner, self.outer, self.inner = 0, 0, 0, 0
-
         # ODME object:
         self.odme = odme
 
+        # Statistics depending on each iterations
+        self.iteration_stats = pd.DataFrame(columns=self.ITERATION_COLS)
+        # Information on factors to be logged with implicit ordering by ODME classes
+        self.current_factors = None
+
+        # Statistics depending on each link
+        self.link_stats = pd.DataFrame(columns=self.LINK_COLS)
+
+        # Iteration number data:
+        self.total_iter, self.outer, self.inner = 0, 0, 0
+
         # Time data for logging information
         self.total_time = 0
+        self.loop_time = None
         self.time = None
 
     # Statistics:
@@ -69,13 +73,19 @@ class ODMEResults(object):
 
         return pd.concat(cumulative_factors, ignore_index=True)
 
-    def get_all_statistics(self) -> pd.DataFrame:
+    def get_iteration_statistics(self) -> pd.DataFrame:
         """
-        Returns dataframe of all assignment values across iterations,
-        along with other statistical information (see self.FACTOR_COLS) 
-        per iteration, per count volume.
+        Returns dataframe of all statistics relevant to each iteration.
+        See self.ITERATION_COLS.
         """
-        return pd.concat(self.statistics, ignore_index=True)
+        return pd.concat(self.iteration_stats, ignore_index=True)
+
+    def get_link_statistics(self) -> pd.DataFrame:
+        """
+        Returns dataframe of all statistics relevant to each link.
+        See self.LINK_COLS.
+        """
+        return pd.concat(self.link_stats, ignore_index=True)
 
     def log_iter(self, iter_type: int) -> None:
         """
@@ -83,14 +93,15 @@ class ODMEResults(object):
 
         Parameters:
             iter_type: the type of iteration to log
+
+        NEEDS UPDATING TO ALLOW FOR US TO RECORD FACTOR STATS AT APPROPRIATE TIMES!!!
         """
         if iter_type == self.INNER:
-            self.__increment_inner()
+            self.__prepare_inner()
         elif iter_type == self.OUTER:
-            self.__increment_outer()
+            self.__prepare_outer()
         elif iter_type == self.FINAL_LOG:
-            self.outer += 1
-            self.inner = 0
+            self.__prepare_final()
         else:
             raise ValueError(
                 f"\'{iter_type}\' is not a valid type of iteration!"
@@ -103,31 +114,63 @@ class ODMEResults(object):
         Computes statistics regarding previous iteration and stores them in the statistics list.
         """
         # Compute Timing:
+        self.__update_times()
+
+        # Update Iteration Statistics
+        self.__update_iteration_stats()
+
+        # Update Link Statistics
+        self.__update_link_stats()
+
+    def __update_iteration_stats(self) -> None:
+        """
+        Appends the newest set of statistics for the last iteration.
+        """
+        # Create Data:
+        for cls_name, factor_stats in zip(self.odme.class_names, self.current_factors):
+            data = dict()
+            
+            data["class"] = [cls_name]
+            data["Outer Loop #"] = [self.outer]
+            data["Inner Loop #"] = [self.inner]
+            data["Total Iteration #"] = [self.total_iter]
+            data["Total Run Time (s)"] = [self.total_time]
+            data["Loop Time (s)"] = [self.loop_time]
+            data["Convergence"] = [self.odme.last_convergence]
+            data["Inner Convergence"] = [self.odme.convergence_change]
+            data["Flow Objective"] = [self.odme.flow_obj]
+            data["Reg Objective"] = [self.odme.reg_obj] # Only relevant for reg_spiess
+            data["mean_factor"] = factor_stats["mean_factor"]
+            data["median_factor"] = factor_stats["median_factor"]
+            data["std_deviation_factor"] = factor_stats["std_deviation_factor"]
+            data["variance_factor"] = factor_stats["variance_factor"]
+            data["min_factor"] = factor_stats["min_factor"]
+            data["max_factor"] = factor_stats["max_factor"]
+
+            # Add the new row of statistics
+            self.iteration_stats.append(pd.DataFrame(data))
+
+    def __update_times(self):
+        """
+        Updates the times for the last iteration.
+        """
         old_time = self.time
         self.time = time.time()
-        loop_time = self.time - old_time
-        self.total_time += loop_time
+        self.loop_time = self.time - old_time
+        self.total_time += self.loop_time
 
-        # Create Data:
+    def __update_link_stats(self) -> None:
+        """
+        Appends the newest set of link statistics.
+        """
         data = self.odme.count_volumes.copy(deep=True)
-        data["Loop Time (s)"] = [loop_time for _ in range(len(data))]
-        data["Total Run Time (s)"] = [self.total_time for _ in range(len(data))]
-        data["Convergence"] = [self.odme.last_convergence for _ in range(len(data))]
-        data["Inner Convergence"] = [self.odme.convergence_change for _ in range(len(data))]
-        data["Flow Objective"] = [self.odme.flow_obj for _ in range(len(data))]
-        data["Reg Objective"] = [self.odme.reg_obj for _ in range(len(data))]
-
-        data["Total Iteration #"] = [self.total_iter for _ in range(len(data))]
-        data["Outer Loop #"] = [self.outer for _ in range(len(data))]
-        data["Inner Loop #"] = [self.inner for _ in range(len(data))]
-
+        data[ "Outer Loop #"] = [self.outer for _ in len(data)]
+        data["Inner Loop #"] = [self.inner for _ in len(data)]
         data["Assigned - Observed"] = (
             self.odme.count_volumes['assign_volume'].to_numpy() -
             self.odme.count_volumes["obs_volume"].to_numpy()
             )
-
-        # Add data to current list of dataframes
-        self.statistics.append(data)
+        self.link_stats.append(data)
 
     def record_factor_stats(self, factors: list[np.ndarray]) -> None:
         """
@@ -135,25 +178,16 @@ class ODMEResults(object):
         factor statistics per iteration per class).
         """
         # Create statistics on all new factors:
-        data = []
-        for cls_name, factor in zip(self.odme.class_names, factors):
-            data.append([
-                cls_name,
-                self.outer,
-                self.inner,
-                self.total_inner,
-                np.mean(factor),
-                np.median(factor),
-                np.std(factor),
-                np.var(factor),
-                np.sum(factor),
-                np.min(factor),
-                np.max(factor)
-            ])
-        new_stats = pd.DataFrame(data, columns=self.FACTOR_COLS)
-
-        # Add the new data to the current list of factor statistics
-        self.factor_stats = pd.concat([self.factor_stats, new_stats], ignore_index=True)
+        self.current_factors = []
+        for factor in factors:
+            self.current_factors.append({
+                'mean_factor' : np.mean(factor),
+                'median_factor': np.median(factor),
+                'std_deviation_factor' : np.std(factor),
+                'variance_factor' : np.var(factor),
+                'min_factor' : np.min(factor),
+                'max_factor' : np.max(factor)
+            })
 
     # Extra Utilities:
     def init_timer(self) -> None:
@@ -164,19 +198,41 @@ class ODMEResults(object):
         """
         self.time = time.time()
 
-    def __increment_outer(self) -> None:
+    def __prepare_outer(self) -> None:
         """
-        Increments outer iteration number, increments total iterations and zeros inner
-        iteration number.
+        Prepares logging of outer iteration
         """
         self.outer += 1
         self.inner = 0
         self.total_iter += 1
+        self.__reset_current_factors()
 
-    def __increment_inner(self) -> None:
+    def __prepare_inner(self) -> None:
         """
-        Increments inner iteration number and total iteration and total inner iteration number.
+        Prepares logging of inner iteration
         """
         self.inner += 1
         self.total_iter += 1
-        self.total_inner += 1
+
+    def __prepare_final(self) -> None:
+        """
+        Prepares logging of final iteration
+        """
+        self.outer += 1
+        self.inner = 0
+        self.__reset_current_factors()
+
+    def __reset_current_factors(self) -> None:
+        """
+        Resets the set of current factors to be missing values.
+        """
+        self.current_factors = []
+        for _ in self.odme.classes:
+            self.current_factors.append({
+                    'mean_factor' : None,
+                    'median_factor': None,
+                    'std_deviation_factor' : None,
+                    'variance_factor' : None,
+                    'min_factor' : None,
+                    'max_factor' : None
+                })
