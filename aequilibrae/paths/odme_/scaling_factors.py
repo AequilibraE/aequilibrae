d7adc813bec0ec4f8448273397b83d55ac12080e
mitsudz <MittunS2003@gmail.com>
2024-02-05 04:39:45 +0000

    Cleaned up ScalingFactors - 6 todo's 5 of which are referencing.

--- aequilibrae/paths/odme_/scaling_factors.py
+++ aequilibrae/paths/odme_/scaling_factors.py
@@ -7,11 +7,16 @@ import numpy as np
 import scipy.stats as spstats
 
 class ScalingFactors(object):
-    """ ODME Algorithms (Scaling Factor Generation) """
+    """ ODME Algorithms (Scaling Factor Generation) 
+    
+    Class should not need to be used by users, only developers.
+    To add a new algorithm simply add it to the ALL_ALGORITHMS list here and 
+    in the ODME class, and then update __set_algorithm and ensure your method
+    output a list of factor matrices for each input demand matrix as per speciciations. 
+    """
     ALL_ALGORITHMS = ["gmean", "spiess", "reg_spiess"]
 
-    # FIGURE OUT TYPEHINT FOR ODME
-    def __init__(self, odme, algorithm: str) -> None:
+    def __init__(self, odme: 'ODME', algorithm: str) -> None:
         """
         Initialises necessary fields from odme object in order to generate
         a set of scaling matrices for the current iteration of the odme 
@@ -21,22 +26,31 @@ class ScalingFactors(object):
             odme: the ODME object containing all fields pertaining to the odme procedure
             algorithm: the algorithm to use to generate scaling factors.
         """
+        # Set the algorithm to be used
         self.algo_name = algorithm
         self.__set_algorithm()
 
+        # Partition count volumes per class for ease of use
         self._c_v = odme.count_volumes
         self.class_names = odme.class_names
         self._class_counts = {
             name : self._c_v[self._c_v['class'] == name].reset_index(drop=True)
             for name in self.class_names
             }
+        
+        # Extra Data for Convenience
         self.names_to_indices = odme.names_to_indices
+
+        # Matrix Data
         self._sl_matrices = odme._sl_matrices
         self.demand_matrices = odme.demands
         self.original_demands = odme.original_demands
+
+        # Algorithm Specific Data
         if algorithm in ["reg_spiess"]:
             self._alpha, self._beta = odme.alpha, odme.beta
 
+        # The ODME object itself
         self.odme = odme
 
     def __set_algorithm(self) -> None:
@@ -52,7 +66,7 @@ class ScalingFactors(object):
         elif self.algo_name == "reg_spiess":
             self._algorithm = self.__reg_spiess
 
-        else:
+        else: # Should never be called - should be dealt with in ODME class
             raise ValueError(
                 f"Invalid algorithm name: {self.algo_name}"
                 "Valid algorithms are: "
@@ -128,13 +142,13 @@ class ScalingFactors(object):
         # Step 3:
         return scaling_factors
 
-    # spiess (Gradient Descent - Objective Function (2,0))
+    # spiess (REFERENCE HERE) - TODO
+    
+    # TODO - add reference to Spiess and above
     def __spiess(self) -> list[np.ndarray]:
         """
         Calculates scaling factor based on gradient descent method via SL matrix,
         assigned flow & observed flows as described by Spiess (1990) - REFERENCE HERE
-
-        MULTI-CLASS UNDER DEVELOPMENT!
         """
         # Derivative matrices for spiess algorithm:
         gradient_matrices = self.__get_derivative_matrices_spiess()
@@ -148,7 +162,8 @@ class ScalingFactors(object):
             for step, gradient in zip(step_sizes,gradient_matrices)
         ]
         return scaling_factors
-
+    
+    # TODO - add reference to Spiess
     def __get_derivative_matrices_spiess(self) -> list[np.ndarray]:
         """
         Returns derivative matrix (see Spiess (1990) - REFERENCE HERE)
@@ -169,19 +184,21 @@ class ScalingFactors(object):
 
         return derivatives
 
+    # TODO - add reference to Spiess
     def __get_step_sizes_spiess(self, gradients: list[np.ndarray]) -> list[float]:
         """
-        Returns estimate of optimal step size (see Spiess (1990) - REFERENCE HERE)
+        Returns estimate of optimal step size (see Spiess (1990) - REFERENCE HERE 
+        and technical documentation)
 
         Parameters:
             gradients: The previously calculated gradient matrices - required for calculating 
                 derivative of link flows with respect to step size.
-
-        MULTI-CLASS UNDER DEVELOPMENT!
         """
+        # Note, we could reduce the number of bounds we need to calculate
+        # (see technical documentation) - however this likely doesn't give rise to
+        # any significant speed increase.
         all_bounds = self.__get_step_size_limits_spiess(gradients)
 
-        # SOME MINOR OPTIMISATIONS CAN BE DONE HERE IN TERMS OF WHAT PRECISELY TO CALCULATE:
         # Calculate step-sizes (lambdas) for each gradient matrix:
         lambdas = []
         for bounds, user_class, gradient in zip(all_bounds, self.class_names, gradients):
@@ -206,16 +223,15 @@ class ScalingFactors(object):
 
     def __get_flow_derivatives_spiess(self,
         user_class: str,
-        gradient: np.ndarray) -> np.ndarray:
+        gradient: np.ndarray
+        ) -> np.ndarray:
         """
-        Returns an array of flow derivatives (v_a' in paper in SMP Teams)
+        Returns an array of flow derivatives (v_a' in technical documentation)
         for the particular class.
 
         Parameters:
             user_class: the name of the class from which to find flow derivatives
             gradient: the gradient for the relevant class
-
-        NOTE - THINK ABOUT RENAMING FOR CONSISTENCY
         """
         data = self._class_counts[user_class]
         demand = self.demand_matrices[self.names_to_indices[user_class]]
@@ -257,19 +273,18 @@ class ScalingFactors(object):
         else:
             return value # Bounds Not Violated
 
+    # TODO - add reference
     def __get_step_size_limits_spiess(self,
-            gradients: list[np.ndarray]) -> list[Tuple[float, float]]:
+            gradients: list[np.ndarray]
+            ) -> list[Tuple[float, float]]:
         """
         Returns bounds for step size in order of upper bound, then lower bound (see Spiess
-        (1990) - REFERENCE HERE) for each gradient matrix.
+        (1990) - REFERENCE HERE and technical documentation) for each gradient matrix.
 
         Parameters:
             gradient: The currently calculating gradient matrix - required for calculating 
                 derivative of link flows with respect to step size.
-
-        MULTI-CLASS UNDER DEVELOPMENT!
         """
-        # THIS CAN BE SIGNIFICANTLY SIMPLIFIED - SEE NOTES
         bounds = []
         # Create each bound and check for edge cases with empty sets:
         for demand, gradient in zip(self.demand_matrices, gradients):
@@ -291,18 +306,20 @@ class ScalingFactors(object):
 
         return bounds
 
-    # regularised spiess (Gradient Descent - Objective Function (2,2))
+    # regularised spiess - see technical documentation for details
+    
+    # TODO - generalise fully to multi-class (including within technical
+    # documentation) and experiment/record results to ascertain efficacy 
+    # of algorithm.
     def __reg_spiess(self) -> list[np.ndarray]:
         """
         Calculates scaling factor based on gradient descent method via SL matrix,
         assigned flow & observed flows for the algorithm by Spiess with a
         regularisation term attached.
 
-        NOTE - I SHOULD REALLY CHECK WHETHER THE BOUNDS ARE REALLY UNCHANGING!
-
-        NOT YET IMPLEMENTED!
-        CURRENTLY ONLY IMPLEMENTING FOR SINGLE CLASS!
-        MULTI-CLASS NOT YET IMPLEMENTED!
+        NOTE - multi-class ODME is NOT yet supported for this algorithm,
+        the technical documentation has not been updated to check for certain
+        how this should be implemented for such cases.
         """
         # Derivative matrices for spiess algorithm:
         gradient_matrices = self.__get_derivative_matrices_reg_spiess()
@@ -322,10 +339,9 @@ class ScalingFactors(object):
         """
         Returns derivative matrix (see notes in SMP Teams)
 
-        CURRENTLY ONLY IMPLEMENTING FOR SINGLE CLASS!
-        MULTI-CLASS NOT YET IMPLEMENTED!
-        NOTE - THIS RETURNS A LIST OF GRADIENT MATRICES, BUT IT HAS NOT BEEN DERIVED THAT THIS
-        IS THE APPROPRIATE GRADIENT FOR MULTI-CLASS (EVEN IF IT LIKELY IS THE CASE)
+        NOTE - multi-class ODME is NOT yet supported for this algorithm,
+        the technical documentation has not been updated to check for certain
+        how this should be implemented for such cases.
         """
         spiess_grads = self.__get_derivative_matrices_spiess()
         g_hats = self.original_demands
@@ -341,7 +357,7 @@ class ScalingFactors(object):
 
     def __get_step_sizes_reg_spiess(self, gradients: list[np.ndarray]) -> list[float]:
         """
-        Returns estimate of optimal step size (see paper in SMP Teams Chat)
+        Returns estimate of optimal step size (see technical documentation)
 
         Parameters:
             gradients: The previously calculated gradient matrices - required for calculating 
@@ -349,10 +365,12 @@ class ScalingFactors(object):
                 (see same paper - basically rate of change of objective w.r.t. change in demand
                 across iteration application of 'f').
 
-        REQUIRES TESTING FOR SINGLE CLASS
-        MULTI-CLASS UNDER DEVELOPMENT!
+        NOTE - multi-class ODME is NOT yet supported for this algorithm,
+        the technical documentation has not been updated to check for certain
+        how this should be implemented for such cases. Single class also required more testing
         """
-        # THIS IS THE SAME BOUNDS AS REGULAR SPIESS, MAY WANT TO CONFIRM THIS IS CORRECT
+        # NOTE - as per technical doc, bounds do not change from reg_spiess to spiess for 
+        # single class.
         all_bounds = self.__get_step_size_limits_spiess(gradients)
 
         # Calculate step-sizes (lambdas) for each gradient matrix:
@@ -402,7 +420,7 @@ class ScalingFactors(object):
         user_class:str,
         gradient: np.ndarray) -> np.ndarray:
         """
-        Returns array of 'eta' terms (see paper in SMP Teams chat)
+        Returns array of 'eta' terms (see technical documentation)
         for a given class.
         """
         demand = self.demand_matrices[self.names_to_indices[user_class]]
