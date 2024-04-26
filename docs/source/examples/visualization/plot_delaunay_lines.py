9d63c07c0272a3c812bb8a6a914a9631f8bd688a
Renata Imai <renataakemii@yahoo.co.uk>
2024-04-26 08:59:14 -0300

    style examples

--- docs/source/examples/visualization/plot_delaunay_lines.py
+++ docs/source/examples/visualization/plot_delaunay_lines.py
@@ -12,6 +12,8 @@ We use the Sioux Falls example once again.
 """
 
 # %%
+
+# Imports
 import pandas as pd
 from uuid import uuid4
 from os.path import join
@@ -33,6 +35,7 @@ project = create_example(fldr)
 # Get the Delaunay Lines generation class
 da = DelaunayAnalysis(project)
 
+# %%
 # Let's create the triangulation based on the zones, but we could create based on the network (centroids) too
 da.create_network("zones")
 
@@ -41,6 +44,7 @@ da.create_network("zones")
 demand = project.matrices.get_matrix("demand_omx")
 demand.computational_view(["matrix"])
 
+# %%
 # And we will call it 'delaunay_test'./ It will also be saved in the results_database.sqlite
 da.assign_matrix(demand, "delaunay_test")
 
