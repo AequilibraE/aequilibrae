4a70b59779931fb14309a293b23fcbb232c13911
mitsudz <MittunS2003@gmail.com>
2024-05-10 13:21:03 +1000

    initial to-do's

---
+++ tests/aequilibrae/paths/test_traffic_assignment_pt_preloading.py
@@ -0,0 +1,51 @@
+import random
+import sqlite3
+import string
+from os.path import join, isfile
+from pathlib import Path
+from random import choice
+
+import numpy as np
+import pandas as pd
+import pytest
+
+from aequilibrae import TrafficAssignment, TrafficClass, Graph, Project
+from aequilibrae.utils.create_example import create_example
+from ...data import siouxfalls_project
+
+# TODO:
+# 1) Create test structure for the API for PT Preloading
+# 2) Figure out how to extract PT network data from public_transport.sqlite (NOT GTFS!!!)
+# 3) Modify database structure to store PCE value for a service and have default info
+# 4) Create algorithm to turn PT network data into a pre-load vector (of size no. edges in graph) with no. vehicles * PCE
+# 5) Create modification to current traffic assignment algorithm to reduce capacity at end of each assignment iteration
+# 6) Consider simple example scenarios to test correctness of algorithms
+# 7) Decide whether more complex tests are necessary
+# 8) Performance testing and speed ups in cython
+
+
+@pytest.fixture
+def project(tmp_path):
+    proj = create_example(str(tmp_path / "test_traffic_assignment"), from_model="coquimbo")
+    proj.network.build_graphs()
+    proj.activate()
+    return proj
+
+
+class TestPTPreloaing:
+
+    def test_run(self, project: Project):
+        project.network.build_graphs()
+        graph = project.network.graphs["c"]
+
+        # Get preload info from somewhere
+
+        assig = TrafficAssignment()
+        # Set preload info into assig
+        # assig.set_pt_preload(preload)
+        # Set other assignment parameters
+
+        # Run assignment
+
+        # Check results
+        assert False  # Ensure test fails for now
