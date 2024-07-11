4c132ae31fb5d2fd7be3b59a46f0760c725992f0
Jamie Cook <jamie@outerloop.io>
2024-07-11 13:20:08 +1000

    .

--- docs/source/examples/creating_models/plot_import_gtfs.py
+++ docs/source/examples/creating_models/plot_import_gtfs.py
@@ -1,20 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     cell_metadata_filter: -all
-#     custom_cell_magics: kql
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.11.2
-#   kernelspec:
-#     display_name: venv
-#     language: python
-#     name: python3
-# ---
-
-# %%
 """
 .. _example_gtfs:
 
@@ -25,6 +8,7 @@ In this example, we import a GTFS feed to our model and perform map matching.
 
 We use data from Coquimbo, a city in La Serena Metropolitan Area in Chile.
 """
+
 # %%
 
 # Imports
