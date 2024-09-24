e92dfa7914d226b109192ff526b49f4c1cb14509
Renata Imai <renataakemii@yahoo.co.uk>
2024-09-24 08:05:32 -0300

    .

--- aequilibrae/project/network/link_types.py
+++ aequilibrae/project/network/link_types.py
@@ -13,11 +13,9 @@ class LinkTypes:
 
     .. code-block:: python
 
-        >>> from aequilibrae import Project
+        >>> project = create_example(project_path)
 
-        >>> p = Project.from_path("/tmp/test_project")
-
-        >>> link_types = p.network.link_types
+        >>> link_types = project.network.link_types
 
         # We can get a dictionary of link types in the model
         >>> all_link_types = link_types.all_types()
--- aequilibrae/project/network/network.py
+++ aequilibrae/project/network/network.py
@@ -140,26 +140,13 @@ class Network:
 
             >>> from aequilibrae import Project
 
-            >>> p = Project()
-            >>> p.new("/tmp/new_project")
-
-            # We now choose a different overpass endpoint (say a deployment in your local network)
-            >>> par = Parameters()
-            >>> par.parameters['osm']['overpass_endpoint'] = "http://192.168.1.234:5678/api"
-
-            # Because we have our own server, we can set a bigger area for download (in M2)
-            >>> par.parameters['osm']['max_query_area_size'] = 10000000000
-
-            # And have no pause between successive queries
-            >>> par.parameters['osm']['sleeptime'] = 0
-
-            # Save the parameters to disk
-            >>> par.write_back()
+            >>> project = Project()
+            >>> project.new(project_path)
 
             # Now we can import the network for any place we want
-            # p.network.create_from_osm(place_name="my_beautiful_hometown")
+            >>> project.network.create_from_osm(place_name="my_beautiful_hometown") # doctest: +SKIP
 
-            >>> p.close()
+            >>> project.close()
         """
 
         if self.count_links() > 0:
@@ -293,15 +280,14 @@ class Network:
             **modes** (:obj:`list`, *Optional*): When working with very large graphs with large number of fields in the
             database, it may be useful to generate only those we need
 
-        To use the *fields* parameter, a minimalistic option is the following
+        To use the 'fields' parameter, a minimalistic option is the following
 
         .. code-block:: python
 
-            >>> from aequilibrae import Project
+            >>> project = create_example(project_path)
 
-            >>> p = Project.from_path("/tmp/test_project")
             >>> fields = ['distance']
-            >>> p.network.build_graphs(fields, modes = ['c', 'w'])
+            >>> project.network.build_graphs(fields, modes = ['c', 'w'])
 
         """
         from aequilibrae.paths import Graph
