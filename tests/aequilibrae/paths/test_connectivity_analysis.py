26f3b78c303649828921a6bb857fd9e827e0acc9
Jake-Moss <jake.moss@uqconnect.edu.au>
2024-10-17 17:26:10 +1000

    Add test with some disconnected pairs

--- tests/aequilibrae/paths/test_connectivity_analysis.py
+++ tests/aequilibrae/paths/test_connectivity_analysis.py
@@ -1,6 +1,7 @@
 import os
 import tempfile
 import uuid
+import pandas as pd
 
 from aequilibrae.paths.connectivity_analysis import ConnectivityAnalysis
 from aequilibrae.utils.create_example import create_example
@@ -12,6 +13,7 @@ def test_connectivity_analysis():
     network.build_graphs()
     graph = network.graphs["c"]
     graph.set_graph(cost_field="distance")
+
     graph.block_centroid_flows = False
     conn_test = ConnectivityAnalysis(graph)
     conn_test.execute()
@@ -24,5 +26,23 @@ def test_connectivity_analysis():
 
     # We can only reach the nodes each node is directly connected to
     assert conn_test.disconnected_pairs.shape[0] == graph.num_zones * (graph.num_zones - 1) - graph.network.shape[0]
+
+    graph.block_centroid_flows = False
+    graph.exclude_links([5, 12, 19])  # Disconnect nodes 1, 2, and 6 from the reset of the graph
+    conn_test = ConnectivityAnalysis(graph)
+    conn_test.execute()
+
+    # Construct the disconnected pairs. 1, 2, and 6 are not reachable from anywhere in
+    # the graph other than them selves, but they can reach everywhere else.
+    origins = []
+    destinations = []
+    disconnected = (1, 2, 6)
+    for o in graph.centroids:
+        if o not in disconnected:
+            origins.extend((o,) * len(disconnected))
+            destinations.extend(disconnected)
+    df = pd.DataFrame({"origin": origins, "destination": destinations}).astype("int64")
+
+    pd.testing.assert_frame_equal(conn_test.disconnected_pairs, df)
 
     project.close()
