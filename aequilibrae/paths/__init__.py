837ec86a3a022e766839b569f635ea2c7daab2d1
pveigadecamargo <pveigadecamargo@anl.gov>
2024-10-17 16:50:27 +1000

    import fixing

--- aequilibrae/paths/__init__.py
+++ aequilibrae/paths/__init__.py
@@ -1,27 +1,22 @@
-from aequilibrae.paths.results import *
+from aequilibrae.paths.AoN import (
+    one_to_all,
+    skimming_single_origin,
+    path_computation,
+    update_path_trace,
+)
+from aequilibrae.paths.public_transport import HyperpathGenerating
+
+from aequilibrae.paths.all_or_nothing import allOrNothing
+from aequilibrae.paths.assignment_paths import AssignmentPaths
+from aequilibrae.paths.connectivity_analysis import ConnectivityAnalysis
+from aequilibrae.paths.graph import Graph, TransitGraph
 from aequilibrae.paths.multi_threaded_aon import MultiThreadedAoN
 from aequilibrae.paths.multi_threaded_skimming import MultiThreadedNetworkSkimming
 from aequilibrae.paths.network_skimming import NetworkSkimming
-from aequilibrae.paths.all_or_nothing import allOrNothing
-from aequilibrae.paths.assignment_paths import AssignmentPaths
-from aequilibrae.paths.traffic_class import TrafficClass, TransitClass
-from aequilibrae.paths.traffic_assignment import TrafficAssignment, TransitAssignment
-from aequilibrae.paths.vdf import VDF
-from aequilibrae.paths.graph import Graph, TransitGraph
 from aequilibrae.paths.optimal_strategies import OptimalStrategies
-from aequilibrae.paths.public_transport import HyperpathGenerating
+from aequilibrae.paths.results import *
 from aequilibrae.paths.route_choice import RouteChoice
 from aequilibrae.paths.sub_area import SubAreaAnalysis
-
-from aequilibrae import global_logger
-
-
-try:
-    from aequilibrae.paths.AoN import (
-        one_to_all,
-        skimming_single_origin,
-        path_computation,
-        update_path_trace,
-    )
-except ImportError as ie:
-    global_logger.warning(f"Could not import procedures from the binary. {ie.args}")
+from aequilibrae.paths.traffic_assignment import TrafficAssignment, TransitAssignment
+from aequilibrae.paths.traffic_class import TrafficClass, TransitClass
+from aequilibrae.paths.vdf import VDF
