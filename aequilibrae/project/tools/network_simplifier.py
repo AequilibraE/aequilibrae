272d22cdfe167854820966d51d81645c17f0298c
Renata Imai <renataakemii@yahoo.co.uk>
2025-05-22 15:21:33 -0300

    Update network_simplifier.py

--- aequilibrae/project/tools/network_simplifier.py
+++ aequilibrae/project/tools/network_simplifier.py
@@ -19,8 +19,8 @@ from aequilibrae.utils.interface.worker_thread import WorkerThread
 class NetworkSimplifier(WorkerThread):
     signal = SIGNAL(object)
 
-    def __init__(self, project=None, parentThread=None) -> None:
-        super().__init__(parentThread)
+    def __init__(self, project=None) -> None:
+        super().__init__(None)
 
         self.project = project or get_active_project()
         self.network = self.project.network
