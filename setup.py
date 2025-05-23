76d97ba8ac8eb09887698ea4f4a0f6951e45cb48
pveigadecamargo <pveigadecamargo@anl.gov>
2025-05-23 22:51:24 +1000

    Adds support for Python 3.13

--- setup.py
+++ setup.py
@@ -151,6 +151,7 @@ if __name__ == "__main__":
             "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: 3.11",
             "Programming Language :: Python :: 3.12",
+            "Programming Language :: Python :: 3.13",
             "Topic :: Scientific/Engineering",
         ],
         cmdclass={"build_ext": build_ext},
