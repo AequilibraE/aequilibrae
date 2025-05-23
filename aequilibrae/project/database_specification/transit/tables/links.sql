885d6decdb92b374ad88c08a6f863b3187981601
Pedro Camargo <c@margo.co>
2025-05-23 15:31:37 +1000

    Apply suggestions from code review

--- aequilibrae/project/database_specification/transit/tables/links.sql
+++ aequilibrae/project/database_specification/transit/tables/links.sql
@@ -52,6 +52,7 @@ select AddGeometryColumn( 'links', 'geometry', 4326, 'LINESTRING', 'XY', 1);
 
 --#
 CREATE UNIQUE INDEX idx_link ON links (link_id, period_id);
+CREATE UNIQUE INDEX idx_period_links ON links (period_id);
 
 --#
 SELECT CreateSpatialIndex( 'links' , 'geometry' );
