23b3eb27cce561a6c9e866a56107e14af56ae00d
Pedro Camargo <c@margo.co>
2025-06-30 14:03:35 -0500

    Update aequilibrae/utils/python_signal.py
    Co-authored-by: Jake Moss <jake.moss@uqconnect.edu.au>

--- aequilibrae/utils/python_signal.py
+++ aequilibrae/utils/python_signal.py
@@ -14,7 +14,7 @@ if not missing_tqdm:
     else:
         from tqdm import tqdm  # type: ignore
 
-show_status = os.environ.get("AEQ_SHOW_PROGRESS", "TRUE") == "TRUE" or missing_tqdm
+show_status = os.environ.get("AEQ_SHOW_PROGRESS", "TRUE") == "TRUE" and not missing_tqdm
 
 
 class PythonSignal:  # type: ignore
