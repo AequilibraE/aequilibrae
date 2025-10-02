5f3cfe84da7f3271901b6dd9d4019533fc0fc420
Renata Imai <53949163+r-akemii@users.noreply.github.com>
2025-10-02 13:35:02 -0300

    Update check_documentation_versions.py

--- docs/website/check_documentation_versions.py
+++ docs/website/check_documentation_versions.py
@@ -15,5 +15,5 @@ release_version = pkg_resources.get_distribution("aequilibrae").version
 with open(os.path.join(npth, "docs/source/useful_links/version_history.rst"), mode="r") as f:
     txt = f.read()
 
-print(f"python/V.{release_version}")
-assert f"python/V.{release_version}" in txt
+print(f"python/v{release_version}")
+assert f"python/v{release_version}" in txt
