fd710fa73c4ffa8169c4df341febd33ebb8d7595
Renata Imai <53949163+r-akemii@users.noreply.github.com>
2025-10-10 16:09:17 -0300

    add example

--- aequilibrae/project/database_specification/network/migrations/002_add_scenario_table.py
+++ aequilibrae/project/database_specification/network/migrations/002_add_scenario_table.py
@@ -11,8 +11,8 @@ from aequilibrae.project.project_creation import run_queries_from_sql_file
 def migrate(
     *,
     project_conn: sqlite3.Connection,
-    transit_conn: Optional[sqlite3.Connection],
-    results_conn: Optional[sqlite3.Connection],
+    transit_conn: Optional[sqlite3.Connection] = None,
+    results_conn: Optional[sqlite3.Connection] = None,
 ):
     logger.info("Beginning migration to add scenario support to the main project_database.sqlite")
     schema = pathlib.Path(__file__).parent.parent / "tables" / "scenarios.sql"
