73e4f42fb0d7f91324ea9bb555d2daec89eca276
Jake Moss <jake@outerloop.io>
2025-12-15 09:57:30 +1000

    Disable threaded read in test

--- tests/aeq/project/test_polygon_gridding.py
+++ tests/aeq/project/test_polygon_gridding.py
@@ -10,13 +10,13 @@ def test_geometry_grid():
     pth = Path(__file__).parent / "data"
 
     # Small simple polygon
-    polygon = gpd.read_parquet(pth / "wynnum.parquet").geometry[0]
+    polygon = gpd.read_parquet(pth / "wynnum.parquet", use_threads=False).geometry[0]
     grid = geometry_grid(polygon, 4326)
     assert grid.geometry.area.sum() == pytest.approx(polygon.area, 0.000000001)
     assert grid.shape[0] == 1
 
     # Bigger polygon
-    polygon = gpd.read_parquet(pth / "porto_rico.parquet").geometry[0]
+    polygon = gpd.read_parquet(pth / "porto_rico.parquet", use_threads=False).geometry[0]
     grid = geometry_grid(polygon, 4326)
     assert grid.geometry.area.sum() == pytest.approx(polygon.area, 0.000000001)
     assert grid.shape[0] == 16
