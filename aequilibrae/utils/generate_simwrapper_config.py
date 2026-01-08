f149f3a761ee1692b31d03486eb288409bded8fd
Veslemøy Berge <puttikoserseg@gmail.com>
2026-01-08 16:59:38 +1000

    add check methods, move project setting to init, save output files as class property

--- aequilibrae/utils/generate_simwrapper_config.py
+++ aequilibrae/utils/generate_simwrapper_config.py
@@ -10,14 +10,15 @@ class SimwrapperConfigGenerator:
     minimal manual work.
     """
 
-    def __init__(self, output_dir="simwrapper"):
+    def __init__(self, project, output_dir="simwrapper"):
         """Initialise the config generator and create output directories.
 
         :Arguments:
             **output_dir** (:obj:`str`, *Optional*): Root directory for SimWrapper outputs
         """
-        self.project = None
+        self.project = project
         self.output_dir = Path(output_dir)
+        self.generated_files = {} 
         self._create_directories()
 
     def _create_directories(self):
@@ -25,26 +26,17 @@ class SimwrapperConfigGenerator:
         Create output directory structure for simwrapper
 
         Structure:
-        simwrapper/
-        ├── data/               # Data files referenced by configs
-        │   ├── links.geojson   # Spatial layer for network
-        │   ├── linkstats.csv   # CSV of link properties/metrics
-        │   ├── other_stats.csv # Additional CSV outputs
-        │   └── ...             # Other CSV/GeoJSON as needed
-        ├── dashboard-*.yaml    # Dashboard configuration file(s)
+        PROJECT-DIRECTORY/
+            data/               # Data files referenced by configs
+                linkstats.csv   # CSV of link properties/metrics
+                other_stats.csv # Additional CSV outputs
+                ...             
+            dashboard-*.yaml    # Dashboard configuration file(s)
         """
         self.data_dir = self.output_dir / "data"  # make subcategories
 
         self.output_dir.mkdir(exist_ok=True)  # base
         self.data_dir.mkdir(exist_ok=True)  # data
-
-    def set_project(self, project):
-        """Set project the .yaml is describing.
-
-        :Arguments:
-            project (Project): AequilibraE project instance
-        """
-        self.project = project
 
     def export_network_stats(self):
         """Export basic network stats to csv. currently links and nodes."""
@@ -57,12 +49,18 @@ class SimwrapperConfigGenerator:
         nodes_df = nodes_obj.data
 
         # build stats into a dataframe
-        stats = {"link_count": len(links_df), "node_count": len(nodes_df)}
+        stats = {
+            "link_count": len(links_df), 
+            "node_count": len(nodes_df)
+                 }
+        
         df = pd.DataFrame([stats])
 
         # write output file
         output_file = self.data_dir / "linkstats.csv"
         df.to_csv(output_file, index=False)
+
+        # save stats
 
     def export_project_stats(self):
         """Export project level statistics"""
@@ -70,20 +68,11 @@ class SimwrapperConfigGenerator:
 
     def generate_config(self):
         """Create the SimWrapper .yaml dashboard configuration."""
-        # if no project stop
-        if self.project == None:
-            raise RuntimeError("You need to set the project via set_project(project) first")
-
-        self._ensure_directories()
-
         # export data
         self.export_project_stats()
 
         # build yaml
         # write dashboard.yaml
-
-    def _ensure_directories(self):
-        pass
 
     def _build_dashboard_config(self):
         pass
@@ -94,7 +83,7 @@ class SimwrapperConfigGenerator:
     def _has_links(self):
         """Checks if project has a network with links"""
         return (
-            self.project is None
+            self.project is not None
             and hasattr(self.project, "network")
             and self.project.network.links is not None
             and len(self.project.network.links.data) > 0
@@ -103,11 +92,32 @@ class SimwrapperConfigGenerator:
     def _has_zones(self):
         """Checks if project has a network with nodes"""
         return (
-            self.project is None
+            self.project is not None
             and hasattr(self.project, "network")
             and self.project.network.nodes is not None
             and len(self.project.network.nodes.data) > 0
         )
 
     def _has_assignments(self):
-        pass
+        """Checks if project has a network with assignments"""
+        return (
+            self.project is not None
+            and hasattr(self.project, "network")
+            and self.project.network.assignments is not None
+            and len(self.project.network.assignments.data) > 0
+        )
+
+    def _has_skims(self):
+        """Checks if project has a network with skims"""
+        return (
+            self.project is not None
+            and hasattr(self.project, "network")
+            and self.project.network.skims is not None
+            and len(self.project.network.skims.data) > 0
+        )
+    
+    def _add_to_generated_files(self, key, path):
+        """Add file to self.generated_files"""
+        self.generated_files[key] = Path(path)
+
+    
