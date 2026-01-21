ea862784ab6e13d6a89d70e9aa16fff0d51768e2
Veslemøy Berge <puttikoserseg@gmail.com>
2026-01-21 12:21:26 +1000

    adds simwrapper_panel; ie start on panel classes to simplify config generator

---
+++ aequilibrae/utils/simwrapper_panel.py
@@ -0,0 +1,65 @@
+class SimwrapperPanel():
+    def __init__(self, type, title, height=None, width=None):
+        self.type = type
+        self.title = title
+        self.height = height
+        self.width = width
+
+    def to_dict(self):
+        """returns dictionairy representation of the panel"""
+        panel = {
+            "type": self.type,
+            "title": self.title,
+        }
+
+        if self.height:
+            panel["height"] = self.height
+
+        if self.width:
+            panel["width"] = self.width
+
+        return panel
+
+
+class TilePanel(SimwrapperPanel):
+    def __init__(self, title, dataset, height=None, width=None):
+        super().__init__("tile", title, height=height, width=width)
+        self.dataset = dataset
+
+# my = TilePanel(title, dataset)
+
+class TextPanel(SimwrapperPanel):
+    def __init__(self, title, data, is_file=False, height=None, width=None):
+        super().__init__("text", title, height=height, width=width)
+        self.data = data
+        self.is_file = is_file
+
+    def to_dict(self):
+        panel = super().to_dict()
+
+        if self.is_file:
+            panel["file"] = self.data
+        else:
+            panel["content"] = self.data
+
+# my = TextPanel(title, data=text, is_file=False, height=None, width=None)    
+
+# my = AequilibraEMapPanel(title, database, view, height, width, center, zoom, projection) 
+class AequilibraEMapPanel(SimwrapperPanel):
+    def __init__(self, title, database="project_database.sqlite", view="map", height=None, width=None, 
+                         center=None, zoom=None, projection=None):
+        super().__init__("aequilibrae", title, height=height, width=width)
+
+        self.database = database
+        self.view = view
+        self.center = center
+        self.zoom = zoom
+        self.projection = projection
+
+        self.defaults = None
+        self.extra_databases = None
+
+    def set_defaults(self, defaults_dict):
+        self.defaults = defaults_dict
+
+    
