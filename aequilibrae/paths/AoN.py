a95a46ff9535e082f64e9f697280a2f259dc64c1
Jake Moss <jake@outerloop.io>
2026-02-17 11:22:05 +1000

    Add backwards compatible module aliases

---
+++ aequilibrae/paths/AoN.py
@@ -0,0 +1,3 @@
+# Module alias to support previous imports like import aequilibrae.paths.AoN
+# FIXME: Remove in 2.0
+from .cython.AoN import *  # noqa: F403
---
+++ aequilibrae/paths/public_transport.py
@@ -0,0 +1,3 @@
+# Module alias to support previous imports like import aequilibrae.paths.public_transport
+# FIXME: Remove in 2.0
+from .cython.public_transport import *  # noqa: F403
