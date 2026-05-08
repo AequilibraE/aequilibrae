420ab42f5a2b6ab23091603961f967f42e2ee442
pveigadecamargo <pveigadecamargo@anl.gov>
2026-05-08 17:39:57 +1000

    Moves the shortest path interface to c++

--- aequilibrae/paths/cython/pq_4ary_heap.pyx
+++ aequilibrae/paths/cython/pq_4ary_heap.pyx
@@ -1,7 +1,6 @@
 # cython: boundscheck=False, wraparound=False, embedsignature=False, cdivision=True, initializedcheck=False
 
 include "parameters.pxi"
-from libcpp cimport bool
 from libc.stddef cimport size_t
 
 cdef extern from "../cpp/pq_4ary_heap.hpp" namespace "aequilibrae::paths::cpp" nogil:
@@ -11,7 +10,7 @@ cdef extern from "../cpp/pq_4ary_heap.hpp" namespace "aequilibrae::paths::cpp" n
         IN_HEAP
 
     cdef struct Element:
-        double key
+        DTYPE_t key
         ElementState state
         size_t node_idx
 
@@ -20,12 +19,12 @@ cdef extern from "../cpp/pq_4ary_heap.hpp" namespace "aequilibrae::paths::cpp" n
         size_t size
         size_t* A
         Element* Elements
-        double* keys
+        DTYPE_t* keys
 
     void init_heap(PriorityQueue* pqueue, size_t length) noexcept
     void free_heap(PriorityQueue* pqueue) noexcept
-    void insert(PriorityQueue* pqueue, size_t element_idx, double key) noexcept
-    void decrease_key(PriorityQueue* pqueue, size_t element_idx, double key_new) noexcept
-    double peek(PriorityQueue* pqueue) noexcept
-    bool is_empty(PriorityQueue* pqueue) noexcept
+    void insert(PriorityQueue* pqueue, size_t element_idx, DTYPE_t key) noexcept
+    void decrease_key(PriorityQueue* pqueue, size_t element_idx, DTYPE_t key_new) noexcept
+    DTYPE_t peek(PriorityQueue* pqueue) noexcept
+    bint is_empty "aequilibrae::paths::cpp::is_empty"(PriorityQueue* pqueue) noexcept
     size_t extract_min(PriorityQueue* pqueue) noexcept
