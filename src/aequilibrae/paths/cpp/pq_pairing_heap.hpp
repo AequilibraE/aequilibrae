fe772966b6c573f5f88b4faaa5d6655cd38cf4f6
Jake Moss <jake@outerloop.io>
2026-05-15 17:31:01 +1000

    Format

--- src/aequilibrae/paths/cpp/pq_pairing_heap.hpp
+++ src/aequilibrae/paths/cpp/pq_pairing_heap.hpp
@@ -5,258 +5,215 @@
 
 #include "pq_heap_base.hpp"
 
-namespace aequilibrae::paths::cpp
-{
+namespace aequilibrae::paths::cpp {
 
-    class PairingHeap final : public PriorityQueueBase<PairingHeap>
-    {
-    public:
-        PairingHeap() noexcept = default;
+class PairingHeap final : public PriorityQueueBase<PairingHeap> {
+public:
+  PairingHeap() noexcept = default;
 
-        ~PairingHeap()
-        {
-            free_heap();
-        }
+  ~PairingHeap() { free_heap(); }
 
-    private:
-        friend class PriorityQueueBase<PairingHeap>;
+private:
+  friend class PriorityQueueBase<PairingHeap>;
 
-        void init_heap_impl(std::size_t length) noexcept
-        {
-            free_heap();
+  void init_heap_impl(std::size_t length) noexcept {
+    free_heap();
 
-            length_ = length;
-            size_ = 0;
-            elements_ = length > 0 ? new Element[length] : nullptr;
-            root_ = kNullIdx;
+    length_ = length;
+    size_ = 0;
+    elements_ = length > 0 ? new Element[length] : nullptr;
+    root_ = kNullIdx;
 
-            for (std::size_t i = 0; i < length_; ++i)
-            {
-                initialize_element(i);
-            }
-        }
+    for (std::size_t i = 0; i < length_; ++i) {
+      initialize_element(i);
+    }
+  }
 
-        void alloc_heap_impl(std::size_t length) noexcept
-        {
-            init_heap(length);
-        }
+  void alloc_heap_impl(std::size_t length) noexcept { init_heap(length); }
 
-        void reset_heap_impl() noexcept
-        {
-        }
+  void reset_heap_impl() noexcept {}
 
-        void free_heap_impl() noexcept
-        {
-            delete[] elements_;
+  void free_heap_impl() noexcept {
+    delete[] elements_;
 
-            elements_ = nullptr;
-            length_ = 0;
-            size_ = 0;
-            root_ = kNullIdx;
-        }
+    elements_ = nullptr;
+    length_ = 0;
+    size_ = 0;
+    root_ = kNullIdx;
+  }
 
-        void insert_impl(std::size_t element_idx, double key) noexcept
-        {
-            assert(element_idx < length_);
-            assert(size_ < length_);
-            assert(elements_[element_idx].state == NOT_IN_HEAP);
-            assert(key < kInfinity);
+  void insert_impl(std::size_t element_idx, double key) noexcept {
+    assert(element_idx < length_);
+    assert(size_ < length_);
+    assert(elements_[element_idx].state == NOT_IN_HEAP);
+    assert(key < kInfinity);
 
-            Element &element = elements_[element_idx];
-            element.key = key;
-            element.state = IN_HEAP;
-            element.child = kNullIdx;
-            element.next = kNullIdx;
-            element.prev = kNullIdx;
+    Element &element = elements_[element_idx];
+    element.key = key;
+    element.state = IN_HEAP;
+    element.child = kNullIdx;
+    element.next = kNullIdx;
+    element.prev = kNullIdx;
 
-            root_ = meld_roots(root_, element_idx);
-            ++size_;
-        }
+    root_ = meld_roots(root_, element_idx);
+    ++size_;
+  }
 
-        void decrease_key_impl(std::size_t element_idx, double key_new) noexcept
-        {
-            assert(element_idx < length_);
-            assert(elements_[element_idx].state == IN_HEAP);
-            assert(key_new <= elements_[element_idx].key);
+  void decrease_key_impl(std::size_t element_idx, double key_new) noexcept {
+    assert(element_idx < length_);
+    assert(elements_[element_idx].state == IN_HEAP);
+    assert(key_new <= elements_[element_idx].key);
 
-            elements_[element_idx].key = key_new;
-            if (elements_[element_idx].prev == kNullIdx)
-            {
-                return;
-            }
+    elements_[element_idx].key = key_new;
+    if (elements_[element_idx].prev == kNullIdx) {
+      return;
+    }
 
-            detach(element_idx);
-            root_ = meld_roots(root_, element_idx);
-        }
+    detach(element_idx);
+    root_ = meld_roots(root_, element_idx);
+  }
 
-        double peek_impl() const noexcept
-        {
-            assert(size_ > 0);
-            assert(root_ != kNullIdx);
-            return elements_[root_].key;
-        }
+  double peek_impl() const noexcept {
+    assert(size_ > 0);
+    assert(root_ != kNullIdx);
+    return elements_[root_].key;
+  }
 
-        bool is_empty_impl() const noexcept
-        {
-            return size_ == 0;
-        }
+  bool is_empty_impl() const noexcept { return size_ == 0; }
 
-        std::size_t extract_min_impl() noexcept
-        {
-            assert(size_ > 0);
-            assert(root_ != kNullIdx);
+  std::size_t extract_min_impl() noexcept {
+    assert(size_ > 0);
+    assert(root_ != kNullIdx);
 
-            const std::size_t min_idx = root_;
-            const std::size_t first = elements_[min_idx].child;
+    const std::size_t min_idx = root_;
+    const std::size_t first = elements_[min_idx].child;
 
-            if (first == kNullIdx)
-            {
-                root_ = kNullIdx;
-            }
-            else
-            {
-                const std::size_t second = elements_[first].next;
-                if (second == kNullIdx)
-                {
-                    elements_[first].prev = kNullIdx;
-                    root_ = first;
-                }
-                else
-                {
-                    std::size_t first_node = first;
-                    std::size_t second_node = second;
-                    std::size_t next_pair = elements_[second_node].next;
-                    elements_[first_node].prev = kNullIdx;
-                    elements_[first_node].next = kNullIdx;
-                    elements_[second_node].prev = kNullIdx;
-                    elements_[second_node].next = kNullIdx;
-                    std::size_t accumulator = meld_roots(first_node, second_node);
+    if (first == kNullIdx) {
+      root_ = kNullIdx;
+    } else {
+      const std::size_t second = elements_[first].next;
+      if (second == kNullIdx) {
+        elements_[first].prev = kNullIdx;
+        root_ = first;
+      } else {
+        std::size_t first_node = first;
+        std::size_t second_node = second;
+        std::size_t next_pair = elements_[second_node].next;
+        elements_[first_node].prev = kNullIdx;
+        elements_[first_node].next = kNullIdx;
+        elements_[second_node].prev = kNullIdx;
+        elements_[second_node].next = kNullIdx;
+        std::size_t accumulator = meld_roots(first_node, second_node);
 
-                    while (next_pair != kNullIdx)
-                    {
+        while (next_pair != kNullIdx) {
 #if defined(__GNUC__) || defined(__clang__)
-                        __builtin_prefetch(&elements_[next_pair], 0, 1);
+          __builtin_prefetch(&elements_[next_pair], 0, 1);
 #endif
-                        first_node = next_pair;
-                        second_node = elements_[first_node].next;
-                        elements_[first_node].prev = kNullIdx;
-                        elements_[first_node].next = kNullIdx;
-                        if (second_node == kNullIdx)
-                        {
-                            accumulator = meld_subtree(accumulator, first_node);
-                            break;
-                        }
+          first_node = next_pair;
+          second_node = elements_[first_node].next;
+          elements_[first_node].prev = kNullIdx;
+          elements_[first_node].next = kNullIdx;
+          if (second_node == kNullIdx) {
+            accumulator = meld_subtree(accumulator, first_node);
+            break;
+          }
 
-                        next_pair = elements_[second_node].next;
-                        elements_[second_node].prev = kNullIdx;
-                        elements_[second_node].next = kNullIdx;
-                        const std::size_t paired = meld_roots(first_node, second_node);
-                        accumulator = meld_subtree(accumulator, paired);
-                    }
-
-                    root_ = accumulator;
-                }
-            }
-
-            elements_[min_idx].state = SCANNED;
-            elements_[min_idx].child = kNullIdx;
-            elements_[min_idx].next = kNullIdx;
-            elements_[min_idx].prev = kNullIdx;
-            --size_;
-
-            return min_idx;
+          next_pair = elements_[second_node].next;
+          elements_[second_node].prev = kNullIdx;
+          elements_[second_node].next = kNullIdx;
+          const std::size_t paired = meld_roots(first_node, second_node);
+          accumulator = meld_subtree(accumulator, paired);
         }
 
-        ElementState effective_state_impl(std::size_t element_idx) const noexcept
-        {
-            return elements_[element_idx].state;
-        }
+        root_ = accumulator;
+      }
+    }
 
-        double element_key_impl(std::size_t element_idx) const noexcept
-        {
-            return elements_[element_idx].key;
-        }
+    elements_[min_idx].state = SCANNED;
+    elements_[min_idx].child = kNullIdx;
+    elements_[min_idx].next = kNullIdx;
+    elements_[min_idx].prev = kNullIdx;
+    --size_;
 
-        struct Element
-        {
-            double key;
-            ElementState state;
-            std::size_t child;
-            std::size_t next;
-            std::size_t prev;
-        };
+    return min_idx;
+  }
 
-        static constexpr std::size_t kNullIdx = static_cast<std::size_t>(-1);
+  ElementState effective_state_impl(std::size_t element_idx) const noexcept {
+    return elements_[element_idx].state;
+  }
 
-        std::size_t length_ = 0;
-        std::size_t size_ = 0;
-        Element *elements_ = nullptr;
-        std::size_t root_ = kNullIdx;
+  double element_key_impl(std::size_t element_idx) const noexcept {
+    return elements_[element_idx].key;
+  }
 
-        void initialize_element(std::size_t element_idx) noexcept
-        {
-            elements_[element_idx].key = kInfinity;
-            elements_[element_idx].state = NOT_IN_HEAP;
-            elements_[element_idx].child = kNullIdx;
-            elements_[element_idx].next = kNullIdx;
-            elements_[element_idx].prev = kNullIdx;
-        }
+  struct Element {
+    double key;
+    ElementState state;
+    std::size_t child;
+    std::size_t next;
+    std::size_t prev;
+  };
 
-        std::size_t meld_roots(std::size_t first_root, std::size_t second_root) noexcept
-        {
-            if (first_root == kNullIdx)
-            {
-                return second_root;
-            }
-            if (second_root == kNullIdx)
-            {
-                return first_root;
-            }
+  static constexpr std::size_t kNullIdx = static_cast<std::size_t>(-1);
 
-            std::size_t winner = first_root;
-            std::size_t loser = second_root;
-            if (elements_[second_root].key < elements_[first_root].key)
-            {
-                winner = second_root;
-                loser = first_root;
-            }
+  std::size_t length_ = 0;
+  std::size_t size_ = 0;
+  Element *elements_ = nullptr;
+  std::size_t root_ = kNullIdx;
 
-            const std::size_t old_child = elements_[winner].child;
-            elements_[loser].next = old_child;
-            elements_[loser].prev = winner;
-            if (old_child != kNullIdx)
-            {
-                elements_[old_child].prev = loser;
-            }
-            elements_[winner].child = loser;
-            return winner;
-        }
+  void initialize_element(std::size_t element_idx) noexcept {
+    elements_[element_idx].key = kInfinity;
+    elements_[element_idx].state = NOT_IN_HEAP;
+    elements_[element_idx].child = kNullIdx;
+    elements_[element_idx].next = kNullIdx;
+    elements_[element_idx].prev = kNullIdx;
+  }
 
-        std::size_t meld_subtree(std::size_t root, std::size_t sub) noexcept
-        {
-            return meld_roots(root, sub);
-        }
+  std::size_t meld_roots(std::size_t first_root,
+                         std::size_t second_root) noexcept {
+    if (first_root == kNullIdx) {
+      return second_root;
+    }
+    if (second_root == kNullIdx) {
+      return first_root;
+    }
 
-        void detach(std::size_t element_idx) noexcept
-        {
-            const std::size_t prev = elements_[element_idx].prev;
-            const std::size_t next = elements_[element_idx].next;
+    std::size_t winner = first_root;
+    std::size_t loser = second_root;
+    if (elements_[second_root].key < elements_[first_root].key) {
+      winner = second_root;
+      loser = first_root;
+    }
 
-            if (elements_[prev].child == element_idx)
-            {
-                elements_[prev].child = next;
-            }
-            else
-            {
-                elements_[prev].next = next;
-            }
-            if (next != kNullIdx)
-            {
-                elements_[next].prev = prev;
-            }
-            elements_[element_idx].prev = kNullIdx;
-            elements_[element_idx].next = kNullIdx;
-        }
-    };
+    const std::size_t old_child = elements_[winner].child;
+    elements_[loser].next = old_child;
+    elements_[loser].prev = winner;
+    if (old_child != kNullIdx) {
+      elements_[old_child].prev = loser;
+    }
+    elements_[winner].child = loser;
+    return winner;
+  }
+
+  std::size_t meld_subtree(std::size_t root, std::size_t sub) noexcept {
+    return meld_roots(root, sub);
+  }
+
+  void detach(std::size_t element_idx) noexcept {
+    const std::size_t prev = elements_[element_idx].prev;
+    const std::size_t next = elements_[element_idx].next;
+
+    if (elements_[prev].child == element_idx) {
+      elements_[prev].child = next;
+    } else {
+      elements_[prev].next = next;
+    }
+    if (next != kNullIdx) {
+      elements_[next].prev = prev;
+    }
+    elements_[element_idx].prev = kNullIdx;
+    elements_[element_idx].next = kNullIdx;
+  }
+};
 
 } // namespace aequilibrae::paths::cpp
--- src/aequilibrae/paths/cpp/pq_std_priority_queue_adapter.hpp
+++ src/aequilibrae/paths/cpp/pq_std_priority_queue_adapter.hpp
@@ -9,145 +9,117 @@
 
 #include "pq_heap_base.hpp"
 
-namespace aequilibrae::paths::cpp
-{
+namespace aequilibrae::paths::cpp {
 
-    class StdPriorityQueueAdapter final : public PriorityQueueBase<StdPriorityQueueAdapter>
-    {
-    public:
-        StdPriorityQueueAdapter() noexcept = default;
+class StdPriorityQueueAdapter final
+    : public PriorityQueueBase<StdPriorityQueueAdapter> {
+public:
+  StdPriorityQueueAdapter() noexcept = default;
 
-        ~StdPriorityQueueAdapter()
-        {
-            free_heap();
-        }
+  ~StdPriorityQueueAdapter() { free_heap(); }
 
-    private:
-        friend class PriorityQueueBase<StdPriorityQueueAdapter>;
+private:
+  friend class PriorityQueueBase<StdPriorityQueueAdapter>;
 
-        using Entry = std::pair<double, std::size_t>;
+  using Entry = std::pair<double, std::size_t>;
 
-        std::priority_queue<Entry, std::vector<Entry>, std::greater<Entry>> pq_;
-        std::vector<double> keys_;
-        std::vector<double> extracted_keys_;
-        std::vector<ElementState> states_;
-        std::size_t size_ = 0;
+  std::priority_queue<Entry, std::vector<Entry>, std::greater<Entry>> pq_;
+  std::vector<double> keys_;
+  std::vector<double> extracted_keys_;
+  std::vector<ElementState> states_;
+  std::size_t size_ = 0;
 
-        void init_heap_impl(std::size_t length) noexcept
-        {
-            alloc_heap(length);
-        }
+  void init_heap_impl(std::size_t length) noexcept { alloc_heap(length); }
 
-        void alloc_heap_impl(std::size_t length) noexcept
-        {
-            free_heap();
-            keys_.assign(length, std::numeric_limits<double>::infinity());
-            extracted_keys_.assign(length, std::numeric_limits<double>::infinity());
-            states_.assign(length, ElementState::NOT_IN_HEAP);
-            size_ = 0;
-        }
+  void alloc_heap_impl(std::size_t length) noexcept {
+    free_heap();
+    keys_.assign(length, std::numeric_limits<double>::infinity());
+    extracted_keys_.assign(length, std::numeric_limits<double>::infinity());
+    states_.assign(length, ElementState::NOT_IN_HEAP);
+    size_ = 0;
+  }
 
-        void reset_heap_impl() noexcept
-        {
-            while (!pq_.empty())
-            {
-                pq_.pop();
-            }
-            for (std::size_t i = 0; i < states_.size(); ++i)
-            {
-                states_[i] = ElementState::NOT_IN_HEAP;
-                keys_[i] = std::numeric_limits<double>::infinity();
-                extracted_keys_[i] = std::numeric_limits<double>::infinity();
-            }
-            size_ = 0;
-        }
+  void reset_heap_impl() noexcept {
+    while (!pq_.empty()) {
+      pq_.pop();
+    }
+    for (std::size_t i = 0; i < states_.size(); ++i) {
+      states_[i] = ElementState::NOT_IN_HEAP;
+      keys_[i] = std::numeric_limits<double>::infinity();
+      extracted_keys_[i] = std::numeric_limits<double>::infinity();
+    }
+    size_ = 0;
+  }
 
-        void free_heap_impl() noexcept
-        {
-            while (!pq_.empty())
-            {
-                pq_.pop();
-            }
-            keys_.clear();
-            extracted_keys_.clear();
-            states_.clear();
-            size_ = 0;
-        }
+  void free_heap_impl() noexcept {
+    while (!pq_.empty()) {
+      pq_.pop();
+    }
+    keys_.clear();
+    extracted_keys_.clear();
+    states_.clear();
+    size_ = 0;
+  }
 
-        void insert_impl(std::size_t element_idx, double key) noexcept
-        {
-            keys_[element_idx] = key;
-            states_[element_idx] = ElementState::IN_HEAP;
-            pq_.push({key, element_idx});
-            ++size_;
-        }
+  void insert_impl(std::size_t element_idx, double key) noexcept {
+    keys_[element_idx] = key;
+    states_[element_idx] = ElementState::IN_HEAP;
+    pq_.push({key, element_idx});
+    ++size_;
+  }
 
-        void decrease_key_impl(std::size_t element_idx, double key_new) noexcept
-        {
-            assert(states_[element_idx] == ElementState::IN_HEAP);
-            assert(key_new <= keys_[element_idx]);
-            keys_[element_idx] = key_new;
-            pq_.push({key_new, element_idx});
-        }
+  void decrease_key_impl(std::size_t element_idx, double key_new) noexcept {
+    assert(states_[element_idx] == ElementState::IN_HEAP);
+    assert(key_new <= keys_[element_idx]);
+    keys_[element_idx] = key_new;
+    pq_.push({key_new, element_idx});
+  }
 
-        [[nodiscard]] double peek_impl() noexcept
-        {
-            discard_stale_entries();
-            return pq_.top().first;
-        }
+  [[nodiscard]] double peek_impl() const noexcept { return pq_.top().first; }
 
-        [[nodiscard]] bool is_empty_impl() const noexcept
-        {
-            return size_ == 0;
-        }
+  [[nodiscard]] bool is_empty_impl() const noexcept { return size_ == 0; }
 
-        [[nodiscard]] std::size_t extract_min_impl() noexcept
-        {
-            discard_stale_entries();
-            const Entry top = pq_.top();
-            pq_.pop();
+  [[nodiscard]] std::size_t extract_min_impl() noexcept {
+    discard_stale_entries();
+    const Entry top = pq_.top();
+    pq_.pop();
 
-            const std::size_t idx = top.second;
-            const double key = top.first;
-            states_[idx] = ElementState::SCANNED;
-            extracted_keys_[idx] = key;
-            --size_;
-            return idx;
-        }
+    const std::size_t idx = top.second;
+    const double key = top.first;
+    states_[idx] = ElementState::SCANNED;
+    extracted_keys_[idx] = key;
+    --size_;
+    return idx;
+  }
 
-        [[nodiscard]] ElementState effective_state_impl(std::size_t element_idx) const noexcept
-        {
-            return states_[element_idx];
-        }
+  [[nodiscard]] ElementState
+  effective_state_impl(std::size_t element_idx) const noexcept {
+    return states_[element_idx];
+  }
 
-        [[nodiscard]] double element_key_impl(std::size_t element_idx) const noexcept
-        {
-            if (states_[element_idx] == ElementState::SCANNED)
-            {
-                return extracted_keys_[element_idx];
-            }
-            return keys_[element_idx];
-        }
+  [[nodiscard]] double
+  element_key_impl(std::size_t element_idx) const noexcept {
+    if (states_[element_idx] == ElementState::SCANNED) {
+      return extracted_keys_[element_idx];
+    }
+    return keys_[element_idx];
+  }
 
-        void discard_stale_entries() noexcept
-        {
-            while (!pq_.empty())
-            {
-                const Entry top = pq_.top();
-                const std::size_t idx = top.second;
-                if (states_[idx] != ElementState::IN_HEAP)
-                {
-                    pq_.pop();
-                    continue;
-                }
-                if (top.first != keys_[idx])
-                {
-                    pq_.pop();
-                    continue;
-                }
-                break;
-            }
-        }
-    };
+  void discard_stale_entries() noexcept {
+    while (!pq_.empty()) {
+      const Entry top = pq_.top();
+      const std::size_t idx = top.second;
+      if (states_[idx] != ElementState::IN_HEAP) {
+        pq_.pop();
+        continue;
+      }
+      if (top.first != keys_[idx]) {
+        pq_.pop();
+        continue;
+      }
+      break;
+    }
+  }
+};
 
 } // namespace aequilibrae::paths::cpp
