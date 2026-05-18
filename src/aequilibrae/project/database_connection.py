8e605a801bb8c9f35b278598cadd59c84564b021
EvelynBond <evelynbond1685@gmail.com>
2026-05-18 14:23:27 +1000

    broaded database connection's path inputs

--- src/aequilibrae/project/database_connection.py
+++ src/aequilibrae/project/database_connection.py
@@ -1,15 +1,16 @@
 import sqlite3
 from pathlib import Path
+from os import PathLike
 
 from aequilibrae.context import get_active_project
 from aequilibrae.utils.spatialite_utils import connect_spatialite
 
 
-def database_connection(db_type: str, project_path: str | Path = "") -> sqlite3.Connection:
+def database_connection(db_type: str, project_path: str | PathLike = "") -> sqlite3.Connection:
     return connect_spatialite(database_path(db_type, project_path))
 
 
-def database_path(db_type: str, project_path: str | Path = "") -> Path:
+def database_path(db_type: str, project_path: str | PathLike = "") -> Path:
     project_path: str | Path = project_path or get_active_project().project_base_path
     if db_type == "project" or db_type == "project_database" or db_type == "network":
         db = "project_database"
