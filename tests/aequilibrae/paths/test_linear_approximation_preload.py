cbff0d533413a4fc6d3a35614e59940974fc6f11
Jake Moss <jake@outerloop.io>
2026-07-10 09:55:29 +1000

    Fix test

--- tests/aequilibrae/paths/test_linear_approximation_preload.py
+++ tests/aequilibrae/paths/test_linear_approximation_preload.py
@@ -1,5 +1,6 @@
-import numpy as np
 from types import SimpleNamespace
+
+import numpy as np
 
 from aequilibrae.paths.linear_approximation import LinearApproximation
 
@@ -13,6 +14,7 @@ class DummyVDF:
 def test_stepsize_derivative_uses_fw_total_flow_state():
     assignment = LinearApproximation.__new__(LinearApproximation)
     assignment.cores = 1
+    assignment.threading_threshold = 10000
     assignment.preload = np.array([10.0, 20.0])
     assignment.current_assigned_flow = np.array([3.0, 4.0])
     assignment.fw_total_flow = assignment.current_assigned_flow + assignment.preload
