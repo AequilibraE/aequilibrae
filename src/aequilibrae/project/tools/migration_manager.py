0ed92ed63cd4b9f491437719291a87b56e469827
Jake Moss <jake@outerloop.io>
2026-07-14 11:58:53 +1000

    Type hints

--- src/aequilibrae/project/tools/migration_manager.py
+++ src/aequilibrae/project/tools/migration_manager.py
@@ -1,14 +1,13 @@
+import contextlib
+import logging
 import pathlib
 import sqlite3
 from dataclasses import dataclass
 from enum import IntEnum
-import contextlib
-import logging
-
 from typing import Optional
 
-from aequilibrae.utils.model_run_utils import import_file_as_module
 from aequilibrae.utils.db_utils import AequilibraEConnection
+from aequilibrae.utils.model_run_utils import import_file_as_module
 
 logger = logging.getLogger(__name__)
 
@@ -229,7 +228,7 @@ class MigrationManager:
             for migration in self.migrations.values():
                 migration.mark_as_seen(conn)
 
-    def find_applicable(self, conn: sqlite3.Connection) -> list[tuple[int, MigrationStatus]]:
+    def find_applicable(self, conn: sqlite3.Connection) -> list[Migration]:
         """
         Find all applicable migrations.
 
@@ -245,7 +244,7 @@ class MigrationManager:
         migrations = list(self.status(conn).items())
 
         for i in range(len(migrations)):
-            k, v = migrations[i]
+            _k, v = migrations[i]
             if v == MigrationStatus.MISSING:
                 break
         else:
@@ -264,7 +263,7 @@ class MigrationManager:
     def upgrade(
         self,
         main_conn: str,
-        connections: dict[str, AequilibraEConnection | sqlite3.Connection],
+        connections: dict[str, AequilibraEConnection],
         skip: Optional[set[int]] = None,
     ):
         """
