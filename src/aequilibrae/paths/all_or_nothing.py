8247f07fc83ad56e1da8526034d434008d88ba1e
Jake Moss <jake@outerloop.io>
2026-08-10 13:35:27 +1000

    Rework path file writing

--- src/aequilibrae/paths/all_or_nothing.py
+++ src/aequilibrae/paths/all_or_nothing.py
@@ -5,7 +5,7 @@ from multiprocessing.dummy import Pool as ThreadPool
 import numpy as np
 
 from aequilibrae.matrix.aequilibrae_matrix import AequilibraeMatrix
-from aequilibrae.paths.cython.AoN import aon_parallel, one_to_all
+from aequilibrae.paths.cython.AoN import aon_parallel
 from aequilibrae.paths.cython.parallel_numpy import assign_link_loads
 from aequilibrae.paths.graph import Graph
 from aequilibrae.paths.results import AssignmentResults
@@ -63,16 +63,14 @@ class allOrNothing(WorkerThread):
             (self.graph.num_zones, self.graph.num_zones, self.results.classes["number"])
         )
         with debug_bridge(logger) as bridge:
-            if self.results.save_path_file:
-                self.__execute_pooled(bridge)  # FIXME: remove this, find another way to write out path files
-            else:
-                skipped = aon_parallel(
-                    self.matrix, self.graph, self.results, self.aux_res, self.results.cores, bridge=bridge
-                )
-                self.report.extend(skipped)
+            skipped = aon_parallel(
+                self.matrix, self.graph, self.results, self.aux_res, self.results.cores, bridge=bridge
+            )
+            self.report.extend(skipped)
         val = self.matrix.index.shape[0]
         msg = f"All-or-Nothing - Traffic Class: {self.class_name} - Zones: {val}/{self.matrix.zones}"
         self.signal.emit(["set_text", msg])
+
         # TODO: Multi-thread this sum
         self.results.compact_link_loads = np.sum(self.aux_res.temp_link_loads, axis=0)
         assign_link_loads(
@@ -82,31 +80,3 @@ class allOrNothing(WorkerThread):
             self.results.elementwise_cores,
             self.results.threading_threshold,
         )
-
-    def __execute_pooled(self, bridge):
-        mat = self.matrix.matrix_view
-        pool = ThreadPool(self.results.cores)
-        all_threads = {"count": 0}
-        for orig in self.matrix.index:
-            i = int(self.graph.nodes_to_indices[orig])
-            if np.nansum(mat[i, :, :]) > 0 or self.results.num_skims > 0:
-                if self.graph.fs[i] == self.graph.fs[i + 1]:
-                    self.report.append("Centroid " + str(orig) + " is not connected")
-                else:
-                    pool.apply_async(self.func_assig_thread, args=(orig, all_threads, bridge))
-        pool.close()
-        pool.join()
-
-    def func_assig_thread(self, origin, all_threads, bridge=None):
-        thread_id = threading.get_ident()
-        th = all_threads.get(thread_id, all_threads["count"])
-        if th == all_threads["count"]:
-            all_threads[thread_id] = all_threads["count"]
-            all_threads["count"] += 1
-
-        x = one_to_all(origin, self.matrix, self.graph, self.results, self.aux_res, th, bridge=bridge)
-        self.cumulative += 1
-        if x != origin:
-            self.report.append(x)
-        msg = f"All-or-Nothing - Traffic Class: {self.class_name} - Zones: {self.cumulative}/{self.matrix.zones}"
-        self.signal.emit(["set_text", msg])
