e9e1123f969d5c51363bf2915445706c2cb90a90
Jake Moss <jake@outerloop.io>
2026-09-02 15:38:11 +1000

    Add periods table in migration
    * src/aequilibrae/project/database_specification/network/migrations/005_fix_default_period_delete_trigger.py:

---
+++ src/aequilibrae/project/database_specification/network/migrations/003_add_periods_table.py
@@ -0,0 +1,24 @@
+import logging
+import pathlib
+import sqlite3
+from typing import Optional
+
+from aequilibrae import project
+from aequilibrae.project.project_creation import run_queries_from_sql_file
+
+logger = logging.getLogger(__name__)
+
+
+def migrate(
+    *,
+    project_conn: sqlite3.Connection,
+    transit_conn: Optional[sqlite3.Connection] = None,
+    results_conn: Optional[sqlite3.Connection] = None,
+):
+    logger.info("Beginning migration to add period table to the main project_database.sqlite")
+    if project_conn.execute("PRAGMA table_info(periods)").fetchone() is None:
+        logger.info("Table does not exist, adding")
+        schema = pathlib.Path(__file__).parent.parent / "tables" / "periods.sql"
+        run_queries_from_sql_file(project_conn, schema)
+    else:
+        logger.info("Table already exists")
